8c37ccbe17c4dfd033508fdd48acdbea09d7195b
Martin Johansson <m.johansson@donders.ru.nl>
2020-07-02 11:22:22 +0200

    Create WriteEventsTsvPrac.m
    Generate events.tsv files for practice

---
+++ utility/WriteEventsTsvPrac.m
@@ -0,0 +1,206 @@
+%% Write bids-compatible events.tsv file from motor task log files
+
+%% Collect existing log files and define output .tsv file
+project = '3022026.01';
+Root = strcat('/project/', project);
+RAWDir   = fullfile(Root, 'raw');
+BIDSDir  = fullfile(Root, 'bids');
+BIDS     = spm_BIDS(BIDSDir);
+
+Sub      = spm_BIDS(BIDS, 'subjects', 'task','motor');
+fprintf('Project = %s\n', project)
+
+  % Exclude participants with missing log files
+Sel = true(size(Sub));
+for n = 1:numel(Sub)
+    
+    if strcmp(project, '3024006.01')
+        SearchPath = fullfile(RAWDir, ['sub-' Sub{n}], 'ses-mri01',  '*motor_behav');
+        MotorBehavDir = dir(SearchPath);
+        MotorBehavDir = fullfile(MotorBehavDir.folder, MotorBehavDir.name);
+    elseif strcmp(project, '3022026.01')
+        MotorBehavDir = fullfile(RAWDir, ['sub-' Sub{n}], 'ses-mri01',  'beh');
+    end
+    
+    PracLog    = spm_select('FPList', MotorBehavDir, [Sub{n} '_(p|P)rac1_logfile\.txt$']);
+    if size(PracLog,1) ~= 1
+		Sel(n) = false;
+    end
+    
+end
+
+Sub = Sub(Sel);
+NSub = numel(Sub);
+fprintf('%i subjects excluded\n', length(Sel) - NSub)
+
+% Preallocate
+PracLog = cell(NSub,1);
+OutputFiles = cell(NSub,1);
+
+% Collect log files, also determine handedness and group.
+for n = 1:NSub
+    
+    if strcmp(project, '3024006.01')
+        SearchPath = fullfile(RAWDir, ['sub-' Sub{n}], 'ses-mri01',  '*motor_behav');
+        MotorBehavDir = dir(SearchPath);
+        MotorBehavDir = fullfile(MotorBehavDir.folder, MotorBehavDir.name);
+    elseif strcmp(project, '3022026.01')
+        MotorBehavDir = fullfile(RAWDir, ['sub-' Sub{n}], 'ses-mri01',  'beh');
+    end
+    
+    PracLog{n}    = spm_select('FPList', MotorBehavDir, [Sub{n} '_(p|P)rac1_logfile\.txt$']);
+    OutputFiles{n} = fullfile(BIDSDir, ['sub-' Sub{n}], 'beh', ['sub-' Sub{n} '_task-motor_acq-practice1_events.tsv']);
+  
+end
+
+% Delete pre-existing files
+for n = 1:NSub
+    if exist(OutputFiles{n}, 'file')
+        delete(OutputFiles{n})
+    end
+end
+
+% Set time scale and formatting for events.tsv
+TimeScaleCust = 1e-3;							% Time unit in custom log is 1/1000 sec
+TR = 1;                                     % Repetition time
+formatSpec = '%0.3f';                       % Number of decimals
+Events = {'fixation' 'cue' 'response'};     % Stimulus events
+NEvents = numel(Events);
+
+%% Extract acquisition time of first image
+for a = 1:NSub
+    
+    %% Read custom log file and extract trial data
+    fileID = fopen(PracLog{a}, 'r');
+    Header = textscan(fileID, '%*s%*s%*s%f%*s%f%*s%*s%*s', 1, 'Delimiter','\t', 'ReturnOnError',false);
+    Trials = textscan(fileID, '%f%s%f%f%f%f%f%f%s', 'Delimiter','\t', 'HeaderLines',2, 'ReturnOnError',false);
+    fclose(fileID);
+    NTrials = numel(Trials{1});                         % Number of trials
+    
+    fixation.onsets = Trials{3} * TimeScaleCust;       % Onsets
+    T0 = fixation.onsets(1);
+    fixation.onsets = fixation.onsets - T0;
+    cue.onsets = Trials{4} * TimeScaleCust - T0;
+    response.onsets = Trials{5} * TimeScaleCust - T0;
+    
+    fixation.durations = (Trials{4} - Trials{3}) * TimeScaleCust;       % Durations
+    cue.durations = (Trials{5} - Trials{4}) * TimeScaleCust;
+    response.durations = ([Trials{3}(2:length(fixation.onsets)); 0] - Trials{5}) * TimeScaleCust;
+
+    response.response_time = Trials{6} * TimeScaleCust;     % Reaction_time
+    
+ %% Separate trials into separate 'Fixation', 'Cue', and 'Response' events, follows reference above
+    onset = cell(NTrials*NEvents,1);                   % Preallocate
+    duration = cell(NTrials*NEvents,1);
+    trial_number = cell(NTrials*NEvents,1);
+    event_type = cell(NTrials*NEvents,1);
+    trial_type = cell(NTrials*NEvents,1);
+    response_time = cell(NTrials*NEvents,1); 
+    button_pressed = cell(NTrials*NEvents,1);
+    button_expected = cell(NTrials*NEvents,1);
+    correct_response = cell(NTrials*NEvents,1);
+
+    Count = 1;
+    for i = 1:NTrials
+        onset{Count} = sprintf(formatSpec, fixation.onsets(i));
+        onset{Count+1} = sprintf(formatSpec, cue.onsets(i));
+        onset{Count+2} = sprintf(formatSpec, response.onsets(i));
+    
+        duration{Count,:} = sprintf(formatSpec, fixation.durations(i));
+        duration{Count+1} = sprintf(formatSpec, cue.durations(i));
+        duration{Count+2} = sprintf(formatSpec, response.durations(i));
+    
+        trial_number{Count} = Trials{1}(i);
+        trial_number{Count+1} = Trials{1}(i);
+        trial_number{Count+2} = Trials{1}(i);
+    
+        event_type{Count} = Events{1};
+        event_type{Count+1} = Events{2};
+        event_type{Count+2} = Events{3};
+    
+        trial_type{Count} = Trials{2}{i};
+        trial_type{Count+1} = Trials{2}{i};
+        trial_type{Count+2} = Trials{2}{i};
+    
+        response_time{Count} = 'n/a';
+        response_time{Count+1} = 'n/a';
+        response_time{Count+2} = sprintf(formatSpec, response.response_time(i));
+    
+        button_pressed{Count} = 'n/a';
+        button_pressed{Count+1} = 'n/a';
+        button_pressed{Count+2} = Trials{7}(i);
+    
+        button_expected{Count} = 'n/a';
+        button_expected{Count+1} = 'n/a';
+        button_expected{Count+2} = Trials{8}(i);
+    
+        correct_response{Count} = Trials{9}{i};
+        correct_response{Count+1} = Trials{9}{i};
+        correct_response{Count+2} = Trials{9}{i};
+        
+        Count = Count+3;
+    end
+    
+    %% Certain timings need to be adjusted
+    for n = 1:NTrials*NEvents       % Catch trials where correct_response = 'Hit'
+        if strcmp(event_type{n}, 'cue') && strcmp(trial_type{n}, 'Catch') && strcmp(correct_response{n+1}, 'Hit')
+            duration{n} = 2.000;       % Cue duration to max
+        end
+        if strcmp(event_type{n}, 'response') && strcmp(trial_type{n}, 'Catch') && strcmp(correct_response{n}, 'Hit')
+            onset{n} = sprintf(formatSpec, str2double(onset{n - 1}) + 1); % NOTE! Temporary assignment of onset to enable sorting. Should actually be n/a.
+            duration{n} = 'n/a';
+            response_time{n} = 'n/a';
+        end
+    end
+    for n = 1:NTrials*NEvents       % Ext trials where button_pressed == 0 (Misses incorrectly labeled as Incorrect)
+        if strcmp(event_type{n}, 'response') && strcmp(trial_type{n}, 'Ext') && (button_pressed{n} == 0)
+            correct_response{n-2} = 'Miss';     % Correct response is changed to miss for fixation, cue, and response
+            correct_response{n-1} = 'Miss';
+            correct_response{n} = 'Miss';
+            duration{n-1} = '2';     % Cue duration to max
+            onset{n} = sprintf(formatSpec, str2double(onset{n - 1}) + 1); % NOTE! Temporary assignment of onset to enable sorting. Should actually be n/a.
+            duration{n} = 'n/a';
+        elseif strcmp(event_type{n}, 'response') && strcmp(trial_type{n}, 'Ext') && strcmp(correct_response{n}, 'Miss') % Just in case...
+            duration{n-1} = '2';     % Cue duration to max
+            onset{n} = sprintf(formatSpec, str2double(onset{n - 1}) + 1); % NOTE! Temporary assignment of onset to enable sorting. Should actually be n/a.
+            duration{n} = 'n/a';
+        end
+    end
+    for n = 1:NTrials*NEvents       % Int2/3 trials where correct_response == 'Miss'
+        if strcmp(event_type{n}, 'response') && (strcmp(trial_type{n}, 'Int2') || strcmp(trial_type{n}, 'Int3')) && strcmp(correct_response{n}, 'Miss')
+            duration{n-1} = '2';     % Cue duration to max
+            onset{n} = sprintf(formatSpec, str2double(onset{n - 1}) + 1); % NOTE! Temporary assignment of onset to enable sorting. Should actually be n/a.
+            duration{n} = 'n/a';
+        end
+    end
+    duration{NTrials*NEvents} = 0;     % Fix last response duration (task ends after last response)
+    
+    %% Sort
+    [E, idx] = sortrows(cellfun(@str2double,onset), 'ascend');  % Generate sorting index 'idx'
+    
+    for n = 1:NTrials*NEvents       % Remove temporary onset-values after sorting is done
+        if strcmp(event_type{n}, 'response') && strcmp(trial_type{n}, 'Catch') && strcmp(correct_response{n}, 'Hit')
+            onset{n} = 'n/a';
+        end
+        if strcmp(event_type{n}, 'response') && (strcmp(trial_type{n}, 'Ext') || strcmp(trial_type{n}, 'Int2') || strcmp(trial_type{n}, 'Int3')) && strcmp(correct_response{n}, 'Miss')
+            onset{n} = 'n/a';
+        end
+    end
+    
+    onset = onset(idx);         % Apply sorting index to all variables
+    duration = duration(idx);
+    trial_number = trial_number(idx);
+    event_type = event_type(idx);
+    trial_type = trial_type(idx);
+    response_time = response_time(idx);
+    button_pressed = button_pressed(idx);
+    button_expected = button_expected(idx);
+    correct_response = correct_response(idx);
+
+    %% Write table
+    T = table(onset, duration, trial_number, event_type, trial_type, response_time, button_pressed, button_expected, correct_response);
+    
+    
+    writetable(T, OutputFiles{a}, 'Delimiter', '\t', 'FileType', 'text')
+    
+end
