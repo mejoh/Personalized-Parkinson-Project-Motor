a4ea44314ebc501f14b42f1115484af790394dd1
Martin Johansson <m.johansson@donders.ru.nl>
2020-08-11 13:05:01 +0200

    Update ClinicalVarsDatabase.R
    Added column 'timepoint' rather than write Visit on each variable name

--- R/ClinicalVarsDatabase.R
+++ R/ClinicalVarsDatabase.R
@@ -1,5 +1,6 @@
+ClinicalVarsDatabase <- function(pattern){
+
 library(tidyverse)
-library(tidyjson)
 library(jsonlite)
 library(readtext)
 library(assertthat)
@@ -49,15 +50,18 @@ ImportSubData <- function(dSub, pattern){
 
 # Create a list of subject directories in the pulled-data folder
 dPulledData <- 'P:/3022026.01/pep/pulled-data'
+# START OF PATTERN LOOP
 dSubs <- list.dirs(dPulledData, recursive = FALSE)
+#dSubs <- dSubs[1:30] # For testing
 dSubs <- dSubs[-c(grep('.pepData', dSubs))]
 nSubs <- length(dSubs)
 
+# Define patterns used to search for Castor.<pattern> files
 #pattern <- c('Castor.HomeQuestionnaires1')
-pattern <- c('Castor.Visit1')
+#pattern <- c('Castor.Visit1')
 #pattern <- c('Castor.Visit2')
 
-# Exclude subjects based on pattern
+# Exclude subjects that do not have files matching pattern
 Sel <- rep(TRUE, nSubs)
 for(n in 1:nSubs){
         filelist <- grep(pattern, dir(dSubs[n]))
@@ -70,12 +74,12 @@ for(n in 1:nSubs){
 msg <- paste(sum(Sel), 'subjects remaining')
 print(msg)
 
+# Perform exclusion
 dSubs <- dSubs[Sel]
 nSubs <- length(dSubs)
 
 # Number of vars differs per subject
 # Create one array containing all unique var names
-# Tag the varnames depending on pattern
 VarNames <- c()
 for(n in 1:nSubs){
         dat <- ImportSubData(dSubs[n], pattern)
@@ -84,20 +88,12 @@ for(n in 1:nSubs){
 }
 UniqueVarNames <- unique(VarNames)
 rm(VarNames)
-if(grepl('Castor.HomeQuestionnaires1', pattern, fixed = TRUE)){
-        UniqueVarNames <- paste('HomeQ1',UniqueVarNames,sep = '.')
-}else if(grepl('Castor.Visit1', pattern, fixed = TRUE)){
-        UniqueVarNames <- paste('Visit1',UniqueVarNames,sep = '.')
-}else if(grepl('Castor.Visit2', pattern, fixed = TRUE)){
-        UniqueVarNames <- paste('Visit2',UniqueVarNames,sep = '.')
-}else{
-        stop('Unrecognized pattern, exiting...')
-}
 
 # Initialize the final data frame and name variables
-df <- tibble('1' = rep('NA', nSubs))
+df <- tibble('1' = rep('NA', nSubs))            # < NAs need to be chars for now so that the code below can work
+# Add a bunch of NAs for other vars
 for(i in 1:(length(UniqueVarNames) - 1)){
-        df <- bind_cols(df, tibble('1' = rep('NA', nSubs)))
+        df <- bind_cols(df, tibble(varname = rep('NA', nSubs)))
 }
 colnames(df) <- UniqueVarNames
 
@@ -107,12 +103,22 @@ for(n in 1:nSubs){
         SubVarNames <- colnames(dat)
         for(i in 1:length(SubVarNames)){
                 colidx <- str_which(UniqueVarNames, SubVarNames[i])
-                df[n,colidx] <- unlist(dat[i])  # < Some variables are lists, like dat[77], these will not be adequately represented!!! 
+                df[n,colidx] <- unlist(dat[i])  # < Some variables are lists, like dat[77], these will be incorrectly imported!!! 
         }
 }
 
+# Add column defining timepoint
+if(grepl('Castor.HomeQuestionnaires1', pattern, fixed = TRUE)){
+        timepoint <- 'HQ1'
+}else if(grepl('Castor.Visit1', pattern, fixed = TRUE)){
+        timepoint <- 'V1'
+}else if(grepl('Castor.Visit2', pattern, fixed = TRUE)){
+        timepoint <- 'V2'
+}
+df <- bind_cols(df, tibble('timepoint' = rep(timepoint,nSubs)))
+
 # Turn uninformative characters to NA
-df[df=='NA'] <- NA    # Not available for certain subjects (castor dependencies)
+df[df=='NA'] <- NA    #
 df[df=='?'] <- NA     # Not available for certain subjects (castor dependencies)
 df[df==''] <- NA      # Not filled in
 df[df=='##USER_MISSING_95##'] <- NA
@@ -120,3 +126,8 @@ df[df=='##USER_MISSING_96##'] <- NA
 df[df=='##USER_MISSING_97##'] <- NA
 df[df=='##USER_MISSING_98##'] <- NA
 df[df=='##USER_MISSING_99##'] <- NA
+
+# Return data frame
+df
+
+}
