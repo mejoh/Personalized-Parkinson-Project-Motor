564711ce878730e92aad0c22d804e0c29392b14a
Martin Johansson <m.johansson@donders.ru.nl>
2020-09-18 16:47:22 +0200

    Update CombinedDatabase.R

--- R/CombinedDatabase.R
+++ R/CombinedDatabase.R
@@ -1,5 +1,11 @@
 # Combine motor task performance and clinical variables into a single data frame
 
+CombinedDatabase <- function(){
+
+##### Libraries #####
+library(tidyverse)
+#####
+
 ##### Import data #####
 
 #Clinical vars
@@ -7,14 +13,14 @@ source('M:/scripts/Personalized-Parkinson-Project-Motor/R/ClinicalVarsGenerateDa
 dfClinVars <- ClinicalVarsGenerateDataFrame(rerun = FALSE)
 
 #Motor task
-source('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskDatabase.R')
-dfMotor <- MotorTaskDatabase('3022026.01')
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskGenerateDataFrame.R')
+dfMotor <- MotorTaskGenerateDataFrame(rerun = FALSE)
 dfMotor.wide <- dfMotor %>%
         pivot_wider(names_from = Condition,
                     values_from = c(Response.Time, Percentage.Correct))
 dfMotor.wide <- dfMotor.wide %>%
         mutate(Visit=ifelse(Visit=='ses-Visit1','V1','V3'),
-               timepoint=Visit,
+               timepoint=as.factor(Visit),
                pseudonym=Subject) %>%
         relocate(pseudonym, timepoint) %>%
         select(-c(Subject,Visit,Group))
@@ -48,15 +54,15 @@ cat('Distribution of task labels: ', '\n')
 dist.labels
 
 missing.labels <- summary(df$MriNeuroPsychTask)[3]
-cat('Number of pseudonyms that lack task labels: ', '\n', missing.labels)
+cat('Number of pseudonyms that lack task labels: ', '\n', missing.labels, '\n')
 
 missing.motor.label <- df %>%
-        select(pseudonym, MriNeuroPsychTask, Response.Time_Ext, Percentage.Correct_Ext) %>%
-        filter(is.na(MriNeuroPsychTask) & !is.na(Response.Time_Ext)) %>%
+        select(pseudonym, MriNeuroPsychTask, Response.Time_Ext, Response.Time_Int2, Response.Time_Int3, Percentage.Correct_Ext, Percentage.Correct_Int2, Percentage.Correct_Int3) %>%
+        filter(is.na(MriNeuroPsychTask) & (!is.na(Response.Time_Ext) | !is.na(Response.Time_Int2) | !is.na(Response.Time_Int3) | !is.na(df$Percentage.Correct_Ext) | !is.na(df$Percentage.Correct_Int2) | !is.na(df$Percentage.Correct_Int3))) %>%
         nrow
 cat('Number of subjects with motor task data that lack task labels: ', '\n',
     missing.motor.label, '\n',
-    'Fixing missing labels for subjects with motor task data...')
+    'Fixing missing labels for subjects with motor task data...', '\n')
 
 for(i in 1:nrow(df)){
         if(is.na(df$MriNeuroPsychTask[i]) && (!is.na(df$Response.Time_Ext[i]) | !is.na(df$Response.Time_Int2[i]) | !is.na(df$Response.Time_Int3[i]) | !is.na(df$Percentage.Correct_Ext[i]) | !is.na(df$Percentage.Correct_Int2[i]) | !is.na(df$Percentage.Correct_Int3[i]))){
@@ -67,7 +73,27 @@ for(i in 1:nrow(df)){
 }
 
 missing.labels <- summary(df$MriNeuroPsychTask)[3]
-cat('Number of pseudonyms that lack task labels: ', '\n', missing.labels)
+cat('Number of pseudonyms that lack task labels: ', '\n', missing.labels, '\n')
+
+#####
+
+##### CHECK: Multiple session labels #####
+
+dist.labels <- summary(as.factor(df$MultipleSessions))
+cat('Distribution of multiple sessions labels: ', '\n')
+dist.labels
+
+missing.labels <- summary(df$MultipleSessions)[3]
+cat('Number of pseudonyms that lack multiple sessions labels: ', '\n', missing.labels, '\n')
+
+cat('Fixing multiple sessions labels', '\n')
+for(n in unique(df$pseudonym)){
+        if(length(df[df$pseudonym == n, ]$timepoint) > 1){
+                df[df$pseudonym == n, ]$MultipleSessions <- 'Yes'
+        }else{
+                df[df$pseudonym == n, ]$MultipleSessions <- 'No'
+        }
+}
 
 #####
 
@@ -97,18 +123,23 @@ df.long.motor <- df.long %>%
 
 #####
 
-##### CHECK: Missing data #####
+##### CHECK: Important characteristics and missing data #####
+
+# Unique pseudonyms
+n.pseudos <- length(unique(df.long.motor$pseudonym))
+cat('Number of unique pseudonyms: ', n.pseudos, '\n')
 
 # Timepoint
 cat('Number of subjects per timepoint: ', '\n')
 table(df.long.motor$timepoint) / 3
 
 
+# Check for missing data
 CheckMissing <- function(dataframe, t, v){
         nm <- dataframe %>% filter(timepoint==t) %>% select(any_of(v)) %>% is.na %>% sum / 3
         cat('Missing ', v, 'at timepoint', t, ': ', nm, '\n')
 }
-# Motor task data
+        # Motor task data
 timepoint <- c('V1','V3')
 vars <- c('Response.Time', 'Percentage.Correct')
 for(t in timepoint){
@@ -116,7 +147,7 @@ for(t in timepoint){
         CheckMissing(df.long.motor, t, v)
         }
 }
-# UPDRS3
+        # UPDRS3
 timepoint <- c('V1','V2')
 vars <- c('Up3OfTotal', 'Up3OnTotal', 'Up3OfBradySum', 'Up3OnBradySum')
 for(t in timepoint){
@@ -125,3 +156,7 @@ for(t in timepoint){
         }
 }
 #####
+
+print(df.long.motor)
+
+}
