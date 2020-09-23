48b6d76e9ef8b354bc217af367d04138238cd261
Martin Johansson <m.johansson@donders.ru.nl>
2020-09-23 14:46:59 +0200

    Update AllVarsDescriptives.R
    Added summary stats and demographics
    
    Added plots for outlier detection

--- R/AllVarsDescriptives.R
+++ R/AllVarsDescriptives.R
@@ -1,27 +1,124 @@
 source('M:/scripts/Personalized-Parkinson-Project-Motor/R/CombinedDatabase.R')
 df <- CombinedDatabase()
 
-source("M:/scripts/RainCloudPlots/tutorial_R/R_rainclouds.R")
+#source("M:/scripts/RainCloudPlots/tutorial_R/R_rainclouds.R")
 library(tidyverse)
 library(cowplot)
 
 ##### Summary stats #####
 
-##
-#df %>% group_by(timepoint) %>% summarise(Off = mean(Up3OfTotal, na.rm = TRUE), On = mean(Up3OnTotal, na.rm = TRUE))
-#df %>% group_by(timepoint) %>% summarise(missing = sum(is.na(Up3OfTotal)), n = n())
+# Response times by timepoint and condition
+df %>%
+        filter(timepoint == 'V1' || timepoint == 'V3') %>%
+        group_by(timepoint, Condition) %>%
+        summarise(n=n(),
+                  avg=mean(Response.Time, na.rm = TRUE),
+                  sd=sd(Response.Time, na.rm = TRUE),
+                  se=sd/(sqrt(n)),
+                  lowerCI=(avg-(1.96*se)),
+                  upperCI=(avg+(1.96*se)),
+                  missing=sum(is.na(Response.Time)))
 
-#####
+# Percentage correct by timepoint and condition
+df %>%
+        filter(timepoint == 'V1' || timepoint == 'V3') %>%
+        group_by(timepoint, Condition) %>%
+        summarise(n=n(),
+                  avg=mean(Percentage.Correct, na.rm = TRUE),
+                  sd=sd(Percentage.Correct, na.rm = TRUE),
+                  se=sd/(sqrt(n)),
+                  lowerCI=(avg-(1.96*se)),
+                  upperCI=(avg+(1.96*se)),
+                  missing=sum(is.na(Percentage.Correct)))
 
-##### Widen data frame to accomodate plotting of motor task performance #####
-
-df.wide <- df %>%
+# Up3OfTotal by timepoint and medication
+df %>%
         pivot_wider(names_from = Condition,
-                    values_from = c(Response.Time, Percentage.Correct))
+                    values_from = c(Response.Time, Percentage.Correct)) %>%
+        select(pseudonym, timepoint, Up3OfTotal, Up3OnTotal) %>%
+        pivot_longer(cols = 3:4,
+                     names_to = 'medication',
+                     values_to = 'score') %>%
+        mutate(medication=ifelse(medication=='Up3OfTotal', 'Off', 'On'),
+               medication=as.factor(medication)) %>%
+        filter(timepoint != 'V3') %>%
+        group_by(timepoint, medication) %>%
+        summarise(n=n(),
+                  avg=mean(score, na.rm = TRUE),
+                  sd=sd(score, na.rm = TRUE),
+                  se=sd/(sqrt(n)),
+                  lowerCI=(avg-(1.96*se)),
+                  upperCI=(avg+(1.96*se)),
+                  missing=sum(is.na(score)))
+
+# 1-year delta(Up3OfTotal) by medication
+df %>%
+        pivot_wider(names_from = Condition,
+                    values_from = c(Response.Time, Percentage.Correct)) %>%
+        select(pseudonym, timepoint, Up3OfTotal.1YearProg, Up3OnTotal.1YearProg) %>%
+        pivot_longer(cols = 3:4,
+                     names_to = 'medication',
+                     values_to = 'delta') %>%
+        mutate(medication=ifelse(medication=='Up3OfTotal.1YearProg', 'Off', 'On'),
+               medication=as.factor(medication)) %>%
+        filter(timepoint == 'V2') %>%
+        group_by(timepoint, medication) %>%
+        summarise(n=n(),
+                  avg=mean(delta, na.rm = TRUE),
+                  sd=sd(delta, na.rm = TRUE),
+                  se=sd/(sqrt(n)),
+                  lowerCI=(avg-(1.96*se)),
+                  upperCI=(avg+(1.96*se)),
+                  missing=sum(is.na(delta)))
+
+# Brain activity
+# IN PROGRESS
 
 #####
 
-##### General plotting #####
+##### Demographics #####
+
+df.demo <- df %>%
+        pivot_wider(names_from = Condition,
+                    values_from = c(Response.Time, Percentage.Correct)) %>%
+        select(pseudonym, timepoint, 
+               Age, 
+               Gender, 
+               EstDisDurYears, 
+               TimeToFUYears, 
+               Up3OfHoeYah, 
+               MostAffSide, 
+               PrefHand, 
+               Responding.Hand, 
+               Up3OfRestTremAmpSum) %>%
+        mutate(Resting.Tremor=as.factor(ifelse(Up3OfRestTremAmpSum >= 1, 'Yes','No')))
+
+categorical.vars <- c(2,4,7,8,9,10,12)
+for(i in categorical.vars){
+        print(table(df.demo[,i]))
+}
+continuous.vars <- c(3,5,6)
+for(i in continuous.vars){
+        var <- na.omit(df.demo[ ,i][[1]])
+        varname <- colnames(df.demo[,i])
+        if(varname=='TimeToFUYears'){
+                var <- var[var > 0]
+        }
+        avg <- mean(var, na.rm = TRUE)
+        sd <- sd(var, na.rm = TRUE)
+        se <- sd/sqrt(length(var))
+        lowerCI <- avg-1.96*se
+        upperCI <- avg-1.96*se
+        dat <- c(avg=avg, sd=sd, se=se, lowerCI=lowerCI, upperCI=upperCI)
+        cat(varname, '\n')
+        print(dat)
+        cat('\n')
+}
+        
+
+#####
+
+##### Collection of plotting functions #####
 
 # Box plots for exploring Time x Group interactions (e.g. effect of medication)
 SessionByGroupBoxPlots <- function(dataframe, x, x.ticks, groups){
@@ -114,8 +211,9 @@ SingleSessionBoxDensPlots <- function(dataframe, y, visit){
         plot_grid(title, plots, ncol=1, rel_heights=c(0.1, 1))
         
 }
-y <- c('Up3OfTotal', 'Up3OfBradySum')
-visit = c('V1')
+y <- c('Up3OfTotal.1YearProg', 'Up3OfBradySum.1YearProg',
+       'Up3OfTotal.1YearProg.Perc', 'Up3OfBradySum.1YearProg.Perc')
+visit = c('V2')
 for(n in unique(y)){
         g <- SingleSessionBoxDensPlots(df, n, visit)
         print(g)
@@ -279,6 +377,138 @@ MedSlopesBySubsPlots <- function(dataframe){
 }
 MedSlopesBySubsPlots(df)
 
+
+#####
+
+##### Widen data frame to accomodate plotting of motor task performance #####
+
+df.wide <- df %>%
+        pivot_wider(names_from = Condition,
+                    values_from = c(Response.Time, Percentage.Correct))
+
+#####
+
+##### CHECK: Outliers in relevant variables #####
+
+# Box and density plots for exploring variables from single sessions
+
+SingleSessionBoxDensPlots <- function(dataframe, y, visit){
+        
+        dataframe <- dataframe %>%
+                filter(timepoint == visit)
+        
+        g_box <- ggplot(dataframe, aes_string(x = "''", y = y)) +
+                geom_boxplot(lwd = 1, fill = 'darkgrey', outlier.size = 3) +
+                theme_cowplot(font_size = 25)
+        
+        g_dens <- ggplot(dataframe, aes_string(y)) +
+                geom_density(alpha = 1/2, lwd = 1, fill = 'darkgrey') +
+                theme_cowplot(font_size = 25) +
+                scale_color_brewer(palette = 'Set1') +
+                scale_fill_brewer(palette = 'Set1')
+        
+        plots <- plot_grid(g_box, g_dens, labels = 'AUTO', nrow = 1, ncol = 2)
+        title <- ggdraw() + draw_label(paste(y, '   Timepoint =', visit), fontface='bold', size = 35)
+        plot_grid(title, plots, ncol=1, rel_heights=c(0.1, 1))
+        
+}
+is_outlier <- function(x, coef=1.5) {
+        return(x < quantile(x, 0.25) - coef * IQR(x) | x > quantile(x, 0.75) + coef * IQR(x))
+}
+
+#Visit1
+y <- c('Up3OfTotal', 'Up3OnTotal',
+       'Up3OfBradySum', 'Up3OnBradySum',
+       'EstDisDurYears', 'Age',
+       'Response.Time_Ext', 'Response.Time_Int2', 'Response.Time_Int3',
+       'Percentage.Correct_Ext', 'Percentage.Correct_Int2', 'Percentage.Correct_Int3')
+visit1 = c('V1')
+outlier.pseudo1 <- c()
+for(n in unique(y)){
+        
+        g <- SingleSessionBoxDensPlots(df.wide, n, visit1)
+        print(g)
+        
+        outliers.visit1 <- df.wide %>% 
+                select(pseudonym, one_of(n), timepoint) %>% 
+                filter(timepoint==visit1) %>% 
+                na.omit %>%
+                mutate(outlier = is_outlier(pull(.,var=2))) %>%
+                filter(outlier == TRUE) %>%
+                select(pseudonym, one_of(n), timepoint)
+        print(outliers.visit1)
+        
+        outlier.pseudo1 <- unique(c(outlier.pseudo1, outliers.visit1$pseudonym))
+}
+
+#Visit2
+y <- c('Up3OfTotal', 'Up3OnTotal',
+       'Up3OfBradySum', 'Up3OnBradySum',
+       'Up3OfTotal.1YearProg', 'Up3OfBradySum.1YearProg',
+       'Up3OnTotal.1YearProg', 'Up3OnBradySum.1YearProg',
+       'TimeToFUYears')
+visit2 = c('V2')
+outlier.pseudo2 <- c()
+for(n in unique(y)){
+        
+        g <- SingleSessionBoxDensPlots(df.wide, n, visit2)
+        print(g)
+        
+        outliers.visit2 <- df.wide %>% 
+                select(pseudonym, one_of(n), timepoint) %>% 
+                filter(timepoint==visit2) %>% 
+                na.omit %>%
+                mutate(outlier = is_outlier(pull(.,var=2))) %>%
+                filter(outlier == TRUE) %>%
+                select(pseudonym, one_of(n), timepoint)
+        print(outliers.visit2)
+        
+        outlier.pseudo2 <- unique(c(outlier.pseudo2, outliers.visit2$pseudonym))
+        
+}
+
+#Visit3
+y <- c('Response.Time_Ext', 'Response.Time_Int2', 'Response.Time_Int3',
+       'Percentage.Correct_Ext', 'Percentage.Correct_Int2', 'Percentage.Correct_Int3')
+visit3 = c('V3')
+outlier.pseudo3 <- c()
+for(n in unique(y)){
+        
+        g <- SingleSessionBoxDensPlots(df.wide, n, visit3)
+        print(g)
+        
+        outliers.visit3 <- df.wide %>% 
+                select(pseudonym, one_of(n), timepoint) %>% 
+                filter(timepoint==visit3) %>% 
+                na.omit %>%
+                mutate(outlier = is_outlier(pull(.,var=2))) %>%
+                filter(outlier == TRUE) %>%
+                select(pseudonym, one_of(n), timepoint)
+        print(outliers.visit3)
+        
+        outlier.pseudo3 <- unique(c(outlier.pseudo3, outliers.visit3$pseudonym))
+}
+
+outlier.pseudo.all <- unique(c(outlier.pseudo1, outlier.pseudo2, outlier.pseudo3))
+cat('Total number of outliers among selected variables:', length(outlier.pseudo.all))
+
+#####
+
+##### Exclusion criteria #####
+
+# Poor performance on motor task
+cutoff <- 0.33
+PerformanceCheck <- df.wide %>%
+        select(pseudonym,timepoint, starts_with('Percentage.Correct')) %>%
+        filter(timepoint=='V1' | timepoint == 'V3') %>%
+        drop_na %>%
+        mutate(BelowCutoff = ifelse(Percentage.Correct_Ext < cutoff | Percentage.Correct_Int2 < cutoff | Percentage.Correct_Int3 < cutoff,
+                                    TRUE,FALSE))
+
+PoorPerformance <- PerformanceCheck %>%
+        filter(BelowCutoff == TRUE)
+
+PoorPerformance.pseudos <- pull(PoorPerformance, var = pseudonym)
 
 #####
 
