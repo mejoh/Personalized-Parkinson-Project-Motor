cee429e4dba6e0aef1667072b5f6b4026ff79123
Martin Johansson <m.johansson@donders.ru.nl>
2020-09-28 09:04:26 +0200

    Update MotorTaskGenerateDataFrame.R

--- R/MotorTaskGenerateDataFrame.R
+++ R/MotorTaskGenerateDataFrame.R
@@ -3,9 +3,9 @@ MotorTaskGenerateDataFrame <- function(rerun = FALSE){
         if(rerun == TRUE){
                 source('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskDatabase.R')
                 dfMotor <- MotorTaskDatabase('3022026.01')
-                save.image('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskData_environment1.RData')
+                save.image('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskData_environment2.RData')
         }else{
-                load('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskData_environment1.RData')
+                load('M:/scripts/Personalized-Parkinson-Project-Motor/R/MotorTaskData_environment2.RData')
         }
         
         print(dfMotor)
