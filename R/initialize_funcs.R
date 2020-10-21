b34cc336731a6eff59870fe329468c604578e5f6
Martin Johansson <m.johansson@donders.ru.nl>
2020-10-21 08:14:47 +0200

    Create initialize_funcs.R
    Single script to initialize functions

---
+++ R/initialize_funcs.R
@@ -0,0 +1,3 @@
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/CombinedDatabase.R')
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/reshape_by_medication.R')
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/reshape_by_task.R')
