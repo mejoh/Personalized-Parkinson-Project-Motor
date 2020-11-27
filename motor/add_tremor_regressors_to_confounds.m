3b33fa59c46a4e912d0bfc5275d8ed3c8f402f5b
Martin Johansson <m.johansson@donders.ru.nl>
2020-11-27 09:14:36 +0100

    Update add_tremor_regressors_to_confounds.m
    Tremor is now determined from prepemg output rather than UPDRS III

--- motor/add_tremor_regressors_to_confounds.m
+++ motor/add_tremor_regressors_to_confounds.m
@@ -1,4 +1,4 @@
-% Run 
+% Run after AROMA confounds have been added (otherwise there wont be a regressors2.tsv file
 % Adds tremor regressors to fmriprep confounds file
 function add_tremor_regressors_to_confounds()
 
@@ -7,8 +7,11 @@ fprintf('Processing data in project: %s\n', Project)
 Root = strcat('/project/', Project, '/pep');
 BIDSDir  = fullfile(Root, 'bids');
 FMRIPrep = fullfile(BIDSDir, 'derivatives/fmriprep');
-ClinVars = fullfile(Root, 'ClinVars');
-Prepemg = '/project/3022026.01/analyses/EMG/motor/processing/prepemg/Regressors/ZSCORED';
+% ClinVars = fullfile(Root, 'ClinVars');
+EMGDir = '/project/3022026.01/analyses/EMG/motor';
+Prepemg = fullfile(EMGDir, 'processing/prepemg/Regressors/ZSCORED');
+Automaticdir = fullfile(EMGDir, 'automaticdir');
+Tremor_Check = fullfile(EMGDir, 'manually_checked/Martin/Tremor_check-09-Nov-2020.mat');
 
 Sub = cellstr(spm_select('List', fullfile(BIDSDir), 'dir', '^sub-POM.*'));
 
@@ -22,25 +25,31 @@ for n = 1:numel(Sub)
         dFunc = fullfile(FMRIPrep, Sub{n}, Visit{v}, 'func');
         ConfoundsFile = cellstr(spm_select('FPList', dFunc, [Sub{n}, '.*task-motor_acq-MB6_run-', '.*_desc-confounds_regressors2.tsv']));
         ConfoundsFile = cellstr(ConfoundsFile{size(ConfoundsFile,1)});
-        % UPDRS-III tremor assessment
-        dClin = fullfile(ClinVars, Sub{n}, Visit{v});
-        TremorFile = cellstr(spm_select('FPList', dClin, '.*Motorische_taken_OFF.Updrs_3_deel_3.json'));
+%         % UPDRS-III tremor assessment
+%         dClin = fullfile(ClinVars, Sub{n}, Visit{v});
+%         TremorFile = cellstr(spm_select('FPList', dClin, '.*Motorische_taken_OFF.Updrs_3_deel_3.json'));
+        % Automaticdir
+        AutoClassed = spm_select('FPList', Automaticdir, [Sub{n}, '-', Visit{v}, '-motor-selected-acc.*.jpg']);
         % Prepemg output
-        TAmp = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v} '.*acc.*amplitude.mat']);
-        TLog = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v} '.*acc.*log.mat']);
-        TPow = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v} '.*acc.*power.mat']);
+        TAmp = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v}, '.*acc.*amplitude.mat']);
+        TLog = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v}, '.*acc.*log.mat']);
+        TPow = spm_select('FPList', Prepemg, [Sub{n}, '-', Visit{v}, '.*acc.*power.mat']);
         
         % Exclude participants
         if isempty(ConfoundsFile{1})
             fprintf('Excluding %s:  %s lacks edited fmriprep confound regressors \n', Sub{n}, Visit{v})
             Sel(n) = false;
         end
-        if isempty(TremorFile{1})
-            fprintf('Excluding %s: %s lacks clinical tremor measurement \n', Sub{n}, Visit{v})
-            Sel(n) = false;
+%         if isempty(TremorFile{1})
+%             fprintf('Excluding %s: %s lacks clinical tremor measurement \n', Sub{n}, Visit{v})
+%             Sel(n) = false;
+%         end
+        if isempty(AutoClassed)
+             fprintf('Excluding %s: %s lacks automatic classification of tremor \n', Sub{n}, Visit{v})
+             Sel(n) = false;    
         end
         if isempty(TAmp) || isempty(TLog) || isempty(TPow)
-            fprintf('Excluding %s: %s lacks accelerometer tremor measurement \n', Sub{n}, Visit{v})
+            fprintf('Excluding %s: %s lacks accelerometer tremor regressor \n', Sub{n}, Visit{v})
             Sel(n) = false;
         end
         
@@ -50,7 +59,10 @@ Sub = Sub(Sel);
 NrSub = numel(Sub);
 fprintf('%i participants included for further processing \n', NrSub)
 
-% Re-label AROMA components based on correlation with task regressors
+% Load manual tremor check
+load(Tremor_Check, 'Tremor_check');
+
+% Add tremor regressor to confounds
 for n = 1:NrSub
     Visit = cellstr(spm_select('List', fullfile(BIDSDir, Sub{n}), 'dir', 'ses-Visit[0-9]'));
     for v = 1:numel(Visit)
@@ -59,28 +71,40 @@ for n = 1:NrSub
         dFunc = fullfile(FMRIPrep, Sub{n}, Visit{v}, 'func');
         ConfoundsFile = cellstr(spm_select('FPList', dFunc, [Sub{n}, '.*task-motor_acq-MB6_run-', '.*_desc-confounds_regressors2.tsv']));
         ConfoundsFile = cellstr(ConfoundsFile{size(ConfoundsFile,1)});
+        NewConfoundsFile = {strrep(ConfoundsFile{1}, 'regressors2.tsv', 'regressors3.tsv')};
         % Load confounds
         confounds      = spm_load(ConfoundsFile{1});    % Load confound file
+
+% Determine tremor from MDS-UPDRS III
+%         % UPDRS-III file
+%         dClin = fullfile(ClinVars, Sub{n}, Visit{v});
+%         TremorFile = cellstr(spm_select('FPList', dClin, '.*Motorische_taken_OFF.Updrs_3_deel_3.json'));
+%         % Load UPDRS-III file
+%         Json = fileread(TremorFile{1});
+%         DecodedJson = jsondecode(Json);
+%         % Determine tremor presence
+%         UD = str2double(DecodedJson.crf.Up3OfRAmpArmYesDev);
+%         UnD = str2double(DecodedJson.crf.Up3OfRAmpArmNonDev);
+%         LD = str2double(DecodedJson.crf.Up3OfRAmpLegYesDev);
+%         LnD = str2double(DecodedJson.crf.Up3OfRAmpLegNonDev);
+%         J = str2double(DecodedJson.crf.Up3OfRAmpJaw);
+%         TremorScores = [UD, UnD, LD, LnD, J];
+%         if max(TremorScores) > 0
+%             fprintf('Tremor detected for %s %s, adding to confounds file... \n', Sub{n}, Visit{v})
+%             TremorPresence = true;
+%         else
+%             TremorPresence = false;
+%         end
         
-        % UPDRS-III file
-        dClin = fullfile(ClinVars, Sub{n}, Visit{v});
-        TremorFile = cellstr(spm_select('FPList', dClin, '.*Motorische_taken_OFF.Updrs_3_deel_3.json'));
-        % Load UPDRS-III file
-        Json = fileread(TremorFile{1});
-        DecodedJson = jsondecode(Json);
-        
-        % Determine tremor presence
-        UD = str2double(DecodedJson.crf.Up3OfRAmpArmYesDev);
-        UnD = str2double(DecodedJson.crf.Up3OfRAmpArmNonDev);
-        LD = str2double(DecodedJson.crf.Up3OfRAmpLegYesDev);
-        LnD = str2double(DecodedJson.crf.Up3OfRAmpLegNonDev);
-        J = str2double(DecodedJson.crf.Up3OfRAmpJaw);
-        TremorScores = [UD, UnD, LD, LnD, J];
-        if max(TremorScores) > 0
-            fprintf('Tremor detected for %s %s, adding to confounds file... \n', Sub{n}, Visit{v})
+% Determine tremor presence from manual checks of prepemg output
+        fid = find(contains(Tremor_check.cName,[Sub{n}, '-',Visit{v}]));    % Find subject's visit
+        if(Tremor_check.cVal(fid) == 1)           % Define tremor presence
             TremorPresence = true;
+            fprintf('Including %s %s : labelled as tremor \n', Sub{n}, Visit{v})
+        elseif(Tremor_check.cVal(fid) == 2)
+            fprintf('Skipping %s %s : labelled as uncertain \n', Sub{n}, Visit{v})
         else
-            TremorPresence = false;
+            fprintf('Skipping %s %s : labelled as non-tremor \n', Sub{n}, Visit{v})
         end
         
         if istrue(TremorPresence)
@@ -104,7 +128,9 @@ for n = 1:NrSub
             confounds2.TremorPower_lin = [zeros(ZerosToAdd,1); TPow.R(:,2)];
             confounds2.TremorPower_deriv1 = [zeros(ZerosToAdd,1); TPow.R(:,4)];
             % Write to confounds file
-            writetable(struct2table(confounds2), ConfoundsFile{1}, 'Delimiter', '\t', 'FileType', 'text')
+            confounds2tab = struct2table(confounds2);       % Convert to table
+            confounds2tab = fillmissing(confounds2tab, 'constant', 0);      % Turn NaN to 0. If you dont, writetable will save columns with NaNs as cells, and all others as doubles
+            writetable(confounds2tab, NewConfoundsFile{1}, 'Delimiter', '\t', 'FileType', 'text')
         end
     end
 end
