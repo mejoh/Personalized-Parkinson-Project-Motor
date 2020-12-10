3bad53ed385129657789491cfdf8a4560883409f
Martin Johansson <m.johansson@donders.ru.nl>
2020-12-10 09:08:31 +0100

    Update ZipOrUnzip.m
    Updated for new data organization

--- utility/ZipOrUnzip.m
+++ utility/ZipOrUnzip.m
@@ -1,51 +1,23 @@
-function ZipOrUnzip(ParkinsonOpMaat)
+function ZipOrUnzip()
 
-PIT = '3024006.01';
-POM = '3022026.01';
-if nargin <1 || isempty(ParkinsonOpMaat)
-    Project = PIT;
-else
-    Project = POM;
-end
+BIDSDir = '/project/3022026.01/pep/bids';
+FMRIPREP = fullfile(BIDSDir, 'derivatives', 'fmriprep');
+Sub = cellstr(spm_select('List', fullfile(FMRIPREP), 'dir', '^sub-POM.*'));
+fprintf('Number of subjects found: %i\n', numel(Sub))
 
-Root     = strcat('/project/', Project);
-BIDSDir  = fullfile(Root, 'bids');
-FMRIPrep = fullfile(BIDSDir, 'derivatives/fmriprep');
-BIDS     = spm_BIDS(BIDSDir);
-Sub      = spm_BIDS(BIDS, 'subjects', 'task','motor');
-
-% Skip ambiguous multiple runs/series cases (for now)
-for AmbigSub = spm_BIDS(BIDS, 'subjects', 'run','2', 'task','motor')
-	fprintf('Skipping sub-%s with ambiguous run-2 data\n', char(AmbigSub))
-	Sub(strcmp(char(AmbigSub), Sub)) = [];
-end
-
-Sel = true(size(Sub));
-
-% Skip unfinished frmiprep jobs or subjects with already unzipped images
 for n = 1:numel(Sub)
-	Report = spm_select('FPList', FMRIPrep, ['sub-' Sub{n} '.*\.html$']);
-	if size(Report,1)~=1
-		fprintf('Skipping sub-%s with no fmriprep output\n', Sub{n})
-		disp(Report)
-		Sel(n) = false;
+    Visit = cellstr(spm_select('List', fullfile(FMRIPREP, Sub{n}), 'dir', 'ses-Visit*'));
+    for v = 1:numel(Visit)
+        dFunc = fullfile(FMRIPREP, Sub{n}, Visit{v}, 'func');
+        img = spm_select('FPList', dFunc, '.*task-motor_acq-MB6.*preproc_bold.nii.*');
+        if exist(img, 'file') && contains(img, 'nii.gz')
+            disp(['Unzipping: ' img])
+            gunzip(img, fileparts(img))
+            delete(img)
+        else
+            fprintf('Skipping %s: No image or already unzipped \n', Sub{n})
+        end
     end
-	
-	SrcNii = spm_select('FPList', fullfile(FMRIPrep, ['sub-' Sub{n}], 'func'), ['sub-' Sub{n} '.*task-motor.*_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz$']);
-	if size(SrcNii,1)~=1
-		fprintf('Skipping sub-%s with no zipped fmriprep images\n', Sub{n})
-		Sel(n) = false;
-    end
-end
-
-Sub     = Sub(Sel);
-NrSub   = numel(Sub);
-
-for n = 1:NrSub
-    SrcNii = spm_select('FPList', fullfile(FMRIPrep, ['sub-' Sub{n}], 'func'), ['sub-' Sub{n} '.*task-motor.*_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz$']);
-    disp(['Unzipping: ' SrcNii])
-    gunzip(SrcNii, fileparts(SrcNii))
-    delete(SrcNii)
 end
 
 end
