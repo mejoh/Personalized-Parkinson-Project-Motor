e4180b1391c18e98400bae38987851dcc75ee947
Martin Johansson <m.johansson@donders.ru.nl>
2020-12-16 16:40:54 +0100

    Update initialize_funcs.R

--- R/initialize_funcs.R
+++ R/initialize_funcs.R
@@ -1,3 +1,5 @@
 source('M:/scripts/Personalized-Parkinson-Project-Motor/R/CombinedDatabase.R')
 source('M:/scripts/Personalized-Parkinson-Project-Motor/R/reshape_by_medication.R')
 source('M:/scripts/Personalized-Parkinson-Project-Motor/R/reshape_by_task.R')
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/generate_motor_task_csv.R')
+source('M:/scripts/Personalized-Parkinson-Project-Motor/R/generate_castor_csv.R')
