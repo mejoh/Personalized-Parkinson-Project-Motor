1b398fbb7c164a6dfbcc6a2572fb86a61d5d1e8a
Martin Johansson <m.johansson@donders.ru.nl>
2021-02-23 14:36:46 +0100

    Updates to paired t-tests

--- motor/motor_2ndlevel_PairedTtests.m
+++ motor/motor_2ndlevel_PairedTtests.m
@@ -1,203 +1,84 @@
-function motor_2ndlevel_PairedTtests(Swap)
-
-%% Swap
-
-if nargin<1 || isempty(Swap)
-	Swap = false;               % A left-right swap of the con-images
-end
+function motor_2ndlevel_PairedTtests()
 
 %% Paths
 
 addpath('/home/common/matlab/spm12');
 spm('defaults', 'FMRI');
 
-FDThresh = 1;
+ses = 'ses-Visit1';
+ConList = {'con_0001' 'con_0002' 'con_0003' 'con_0004' 'con_0005' 'con_0006' 'con_0007' 'con_0008' 'con_0009' 'con_0010'};
+ConNames   = {'Ext' 'Int2' 'Int3' 'Catch' 'Int' 'Ext>Int' 'Int>Ext' 'Int3>Int2' 'Int2>Int3' 'Mean_ExtInt'};
 ANALYSESDir = '/project/3022026.01/analyses/motor/DurAvg_ReAROMA_PMOD_TimeDer';
-BIDSDir = '/project/3022026.01/pep/bids';
-BIDSDir_PIT = '/project/3022026.01/pep/bids_PIT';
-Sub = cellstr(spm_select('List', fullfile(ANALYSESDir), 'dir', '^sub-POM.*'));
+% ANALYSESDir = '/project/3022026.01/analyses/motor/fMRI_EventRelated_BRCtrl';
+Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, 'Group', 'con_0001', 'ses-Visit1'), '.*sub-POM.*'));
+Sub = extractBetween(Sub, 1, 31);
 fprintf('Number of subjects processed: %i\n', numel(Sub))
 
 %% Selection
 
-Sel = true(size(Sub));
-% Exclude participants without 1st-level analyses for visit 1
+% Exclude healthy controls
+Sel = false(size(Sub));
 for n = 1:numel(Sub)
-    d = dir(fullfile(ANALYSESDir, Sub{n}, 'ses-Visit1*'));
-    for v = 1:length(d)
-        if ~spm_select('List', fullfile(d(v).folder, d(v).name, '1st_level'), '^con.*\.nii$')
-            Sel(n) = false;
-            fprintf('Excluding %s without previous SPM output for Visit1\n', Sub{n})
-        end
-    end
-end
-% Exclude participants who do not have both POM and PIT Visit1 data
-for n = 1:numel(Sub)
-    d = dir(fullfile(ANALYSESDir, Sub{n}, 'ses-Visit1*'));
-    if ~(sum(contains({d.name}, 'ses-Visit1')) > 1 && sum(contains({d.name}, 'ses-Visit1_PIT')) == 1)
+    if (contains(Sub{n}, 'PD_PIT') || contains(Sub{n}, 'PD_POM'))
+        Sel(n) = true;
+    else
         Sel(n) = false;
-        fprintf('Excluding %s without Visit1 for both POM and PIT\n', Sub{n})
     end
 end
 Sub = Sub(Sel);
 
-% Take the last run for each participant's session(s)
-Run = {};
-Session = {};
-Sub2 = {};
-for n = 1:numel(Sub)
-    Visit = cellstr(spm_select('List', fullfile(ANALYSESDir, Sub{n}), 'dir', 'ses-Visit1*'));
-    for v = 1:numel(Visit)
-        if strcmp(Visit{v}, 'ses-Visit1_PIT')
-            d = BIDSDir_PIT;
-        elseif strcmp(Visit{v}, 'ses-Visit1')
-            d = BIDSDir;
-        end
-        Sub2 = [Sub2; {Sub{n}}];
-        Session = [Session; {Visit{v}}];
-        Run = [Run; {num2str(FindLastRun(d, char(Sub{n}), 'ses-Visit1', 'motor', 'MB6'))}];
-    end
-end
-SubInfo.Sub = Sub2;
-SubInfo.Session = Session;
-SubInfo.Run = Run;
+SubInfo.Sub = extractBetween(Sub, 8, 31);
+SubInfo.Group = extractBetween(Sub, 1, 6);
 
-% Exclude Visit3
-Sel = true(size(SubInfo.Sub));
+% Find patients that are included in both PIT and P
+[~, ind] = unique(SubInfo.Sub);   % indices to unique subs
+duplicate_ind = setdiff(1:size(SubInfo.Sub, 1), ind);   % duplicate indices
+duplicate_subs = SubInfo.Sub(duplicate_ind);   % duplicate values
+
+Sel = true(size(Sub));
 for n = 1:numel(SubInfo.Sub)
-    if contains(SubInfo.Session{n}, 'ses-Visit3')
+    if contains(SubInfo.Sub{n}, duplicate_subs)
+        Sel(n) = true;
+    else
         Sel(n) = false;
-        fprintf('Excluding Visit3 SPM output for %s \n', SubInfo.Sub{n})
     end
 end
-fprintf('%i Visit3 excluded \n', sum(Sel == 0))
 SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Session = SubInfo.Session(Sel);
-SubInfo.Run = SubInfo.Run(Sel);
+SubInfo.Group = SubInfo.Group(Sel);
 
 %% Collect events.json and confound files
 
-JsonFiles = cell(size(SubInfo.Sub));
-ConfFiles = cell(size(SubInfo.Sub));
-
+SubInfo.ConfFiles = cell(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
-    if contains(SubInfo.Session{n}, 'PIT')
-        JsonFiles{n} = spm_select('FPList', fullfile(BIDSDir_PIT, SubInfo.Sub{n}, 'ses-Visit1', 'beh'), ['.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_events\.json$']);
+    if contains(SubInfo.Group{n}, '_PIT')
+        Session = 'ses-Visit1_PIT';
     else
-        JsonFiles{n} = spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, 'ses-Visit1', 'beh'), ['.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_events\.json$']);
+        Session = 'ses-Visit1';
     end
-    ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, SubInfo.Session{n}), ['^.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_desc-confounds_regressors3.mat$']);
-    if isempty(ConfFiles{n})
-        ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, SubInfo.Session{n}), ['^.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_desc-confounds_regressors2.mat$']);
+    SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_regressors3.mat$');
+    if isempty(SubInfo.ConfFiles{n})
+        SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_regressors2.mat$');
     end
-end
-
-%% Group and handedness
-
-Group = strings(size(SubInfo.Sub));
-RespondingHand = strings(size(SubInfo.Sub));
-PercentageCorrect = strings(size(SubInfo.Sub));
-
-for n = 1:numel(SubInfo.Sub)
-    Json = fileread(JsonFiles{n});
-    DecodedJson = jsondecode(Json);
-    Group(n) = DecodedJson.Group.Value;
-    if contains(SubInfo.Session{n}, 'PIT')
-        d = BIDSDir_PIT;
-        Group(n) = FindGroup(d, SubInfo.Sub{n}, 'ses-Visit1', 'dwi');
-    else
-        Group(n) = 'PD_POM';
-    end
-    RespondingHand(n) = DecodedJson.RespondingHand.Value;
-    PercentageCorrect(n) = DecodedJson.ExtCorrResp.Value;
 end
 
 %% Framewise displacement
 
-FD = zeros(size(SubInfo.Sub));
+SubInfo.FD = zeros(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
-    Confounds = spm_load(ConfFiles{n});
+    Confounds = spm_load(SubInfo.ConfFiles{n});
     FrameDisp = Confounds.R(:,strcmp(Confounds.names, 'framewise_displacement'));
     FrameDisp(isnan(FrameDisp)) = 0;
-    FD(n) = mean(FrameDisp);
-end
-
-%% Exclusion
-
-% FD
-Sel = true(size(SubInfo.Sub));
-for n = 1:numel(SubInfo.Sub)
-    if FD(n) > FDThresh
-        idx = contains(SubInfo.Sub, SubInfo.Sub{n});
-        Sel(idx) = false;
-        sprintf('Excluding %s (%s) due to %f mean framewise displacement (Threshold = %f) \n', SubInfo.Sub{n}, Group(n), FD(n), FDThresh)
-    end
-end
-fprintf('%i participants excluded due to excessive motion \n', sum(Sel == 0)/2)
-SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Session = SubInfo.Session(Sel);
-SubInfo.Run = SubInfo.Run(Sel);
-Group = Group(Sel);
-RespondingHand = RespondingHand(Sel);
-FD = FD(Sel);
-PercentageCorrect = PercentageCorrect(Sel);
-
-% Percentage correct responses in external
-Sel = true(size(SubInfo.Sub));
-for n = 1:numel(SubInfo.Sub)
-    if double(PercentageCorrect(n)) <= 0.25
-        idx = contains(SubInfo.Sub, SubInfo.Sub{n});
-        Sel(idx) = false;
-        sprintf('Excluding %s (%s) due to %f percentage correct responses in External (Threshold = 25%) \n', SubInfo.Sub{n}, Group(n), PercentageCorrect(n))
-    end
-end
-fprintf('%i sessions excluded due to poor performance \n', sum(Sel == 0))
-SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Session = SubInfo.Session(Sel);
-SubInfo.Run = SubInfo.Run(Sel);
-Group = Group(Sel);
-RespondingHand = RespondingHand(Sel);
-FD = FD(Sel);
-
-%% Copy and flip
-
-ConList = {'con_0001' 'con_0002' 'con_0003' 'con_0004' 'con_0005' 'con_0006' 'con_0007' 'con_0008' 'con_0009' 'con_0010'};
-ConNames   = {'Ext' 'Int2' 'Int3' 'Catch' 'Int' 'Ext>Int' 'Int>Ext' 'Int3>Int2' 'Int2>Int3' 'Mean_ExtInt'};
-SessionList = {'ses-Visit1' 'ses-Visit1_PIT'};
-for c = 1:numel(ConList)
-    ConDir = fullfile(ANALYSESDir, 'Group', ConList{c});
-    if ~exist(ConDir, 'dir')
-        mkdir(ConDir);
-    else
-        delete(fullfile(ConDir, '*.*'));
-    end
-    for s = 1:numel(SessionList)
-        for n = 1:numel(SubInfo.Sub)
-            if strcmp(SessionList{s}, SubInfo.Session{n})
-                InputConFile = fullfile(ANALYSESDir, SubInfo.Sub{n}, SubInfo.Session{n}, '1st_level', [ConList{c} '.nii']);
-                if exist(InputConFile, 'file')
-                    OutputConFile = fullfile(ConDir, [char(Group(n)) '_' SubInfo.Sub{n} '_' SubInfo.Session{1} '_' ConList{c} '.nii']);
-                    copyfile(InputConFile, OutputConFile)
-                            if strcmp(RespondingHand(n), 'Left') && Swap
-                                fprintf('LR-swapping: %s\n', OutputConFile)
-                                Hdr		  = spm_vol(OutputConFile);
-                                Vol		  = spm_read_vols(Hdr);
-                                Hdr.fname = spm_file(OutputConFile, 'suffix', 'L2Rswap');
-                                spm_write_vol(Hdr, flipdim(Vol,1));		% LR is the first dimension in MNI space
-                                delete(OutputConFile);
-                            end
-                else
-                    fprintf('%s: Confile not available \n', SubInfo.Sub{n})
-                end
-            end
-        end
-    end
+    SubInfo.FD(n) = mean(FrameDisp);
 end
 
 %% Assemble inputs
 
 fprintf('%i subjects will be analyzed. Does this match your job.m file?! \n', numel(unique(SubInfo.Sub)))
-tabulate(Group)
+tabulate(SubInfo.Group)
+if length(SubInfo.Group(strcmp(SubInfo.Group,'PD_PIT'))) ~= length(SubInfo.Group(strcmp(SubInfo.Group, 'PD_POM')))
+    msg = 'Length of groups are not equal, exiting...';
+    error(msg)
+end
 Inputs = cell(49, 1);
 jobs = cell(numel(ConList),1);
 
@@ -213,15 +94,18 @@ for c = 1:numel(ConList)
     
     % Assemble paired scans
     for n = 1:numel(unique(SubInfo.Sub))
-        Img_off = dir(fullfile(ANALYSESDir, 'Group', ConList{c}, ['PD_PIT_' SubInfo.Sub{n} '*' ConList{c} '*.nii']));
+        Img_off = dir(fullfile(ANALYSESDir, 'Group', ConList{c}, ses, ['PD_PIT_' SubInfo.Sub{n} '*' ConList{c} '*.nii']));
         Img_off = strcat(Img_off.folder, '/', Img_off.name);
-        Img_on = dir(fullfile(ANALYSESDir, 'Group', ConList{c}, ['PD_POM_' SubInfo.Sub{n} '*' ConList{c} '*.nii']));
+        Img_on = dir(fullfile(ANALYSESDir, 'Group', ConList{c}, ses, ['PD_POM_' SubInfo.Sub{n} '*' ConList{c} '*.nii']));
         Img_on = strcat(Img_on.folder, '/', Img_on.name);
         Inputs{n+1,1} = {Img_off;Img_on};
     end
     
     % Assemble confound regressors
-    Inputs{49,1} = FD';
+    Inputs{49,1} = SubInfo.FD;
+    
+    filename = char(fullfile(Inputs{1,1}, 'Inputs.mat'));
+    save(filename, 'Inputs')
 
 %% Run
 
