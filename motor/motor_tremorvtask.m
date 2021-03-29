c24a76591bca7b468b6d00cf183dd8a4c5dcc642
Martin Johansson <m.johansson@donders.ru.nl>
2021-03-29 11:52:51 +0200

    Update motor_tremorvtask.m
    added catch

--- motor/motor_tremorvtask.m
+++ motor/motor_tremorvtask.m
@@ -1,5 +1,5 @@
 % Compute correlation between tremor regressor and task conditions
-function [ext, int2, int3, bp] = motor_tremorvtask(spmmat, tremreg)
+function [ext, int2, int3, cat, bp] = motor_tremorvtask(spmmat, tremreg)
 
 if ~exist(spmmat, 'file') || ~exist(tremreg, 'file')
     msg = 'Cannot find SPM matfile or tremor regressor file';
@@ -7,7 +7,7 @@ if ~exist(spmmat, 'file') || ~exist(tremreg, 'file')
 end
 
 load(spmmat);
-regnames = {'Sn(1) Ext*bf(1)', 'Sn(1) Int2*bf(1)', 'Sn(1) Int3*bf(1)', 'Sn(1) ButtonPress*bf(1)'};
+regnames = {'Sn(1) Ext*bf(1)', 'Sn(1) Int2*bf(1)', 'Sn(1) Int3*bf(1)', 'Sn(1) Catch*bf(1)', 'Sn(1) ButtonPress*bf(1)'};
 regids = contains(SPM.xX.name, regnames);
 taskregressors = SPM.xX.X(:,regids);
 
@@ -15,9 +15,10 @@ load(tremreg);
 leftpad = zeros(5,1);
 tremorregressor = [leftpad; R(:,2)];
 
-ext = corr(taskregressors(:,1), tremorregressor);
-int2 = corr(taskregressors(:,2), tremorregressor);
-int3 = corr(taskregressors(:,3), tremorregressor);
-bp = corr(taskregressors(:,4), tremorregressor);
+ext = corr(taskregressors(:,2), tremorregressor);
+int2 = corr(taskregressors(:,3), tremorregressor);
+int3 = corr(taskregressors(:,4), tremorregressor);
+cat = corr(taskregressors(:,1), tremorregressor);
+bp = corr(taskregressors(:,5), tremorregressor);
 
 end
