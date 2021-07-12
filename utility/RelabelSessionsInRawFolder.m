4d40a21381b5ef55d1c82e7fe3812b0817668289
Martin Johansson <m.johansson@donders.ru.nl>
2021-07-12 16:37:51 +0200

    Update RelabelSessionsInRawFolder.m

--- utility/RelabelSessionsInRawFolder.m
+++ utility/RelabelSessionsInRawFolder.m
@@ -1,20 +1,20 @@
-% dRaw = '/project/3024006.01/raw/';
-dRaw = '/project/3022026.01/raw/';
-% Sub = cellstr(spm_select('List', fullfile(dRaw), 'dir', '^sub-PIT.*'));
-Sub = cellstr(spm_select('List', fullfile(dRaw), 'dir', '^sub-POM.*'));
+dRaw = '/project/3024006.01/raw/';
+% dRaw = '/project/3022026.01/raw/';
+Sub = cellstr(spm_select('List', fullfile(dRaw), 'dir', '^sub-PIT2.*'));
+% Sub = cellstr(spm_select('List', fullfile(dRaw), 'dir', '^sub-POM3.*'));
 before = 'ses-';
-after = 'Visit';
-% ProjectTag = 'PIT';
-ProjectTag = 'POM';
+old = 'mri0';
+new = 'PITVisit';
+% new = 'POMVisit';;
 
 for n = 1:numel(Sub)
     d = fullfile(dRaw, Sub{n});     % Subject-specific directory
-    dInfo = dir(fullfile(d, [before after '*']));      % Find folders for sessions 
+    dInfo = dir(fullfile(d, [before 'mri0' '*']));      % Find folders for sessions 
     if ~isempty(dInfo)      % Empty dInfo indicates that sessions have been renamed
         fprintf('Renaming sessions for %s... \n', Sub{n})
         for s = 1:length(dInfo)
-            if contains(dInfo(s).name, [before after])
-                dInfo(s).newname = insertBefore(dInfo(s).name, after, ProjectTag);      % New name for session
+            if contains(dInfo(s).name, [before old])
+                dInfo(s).newname = replace(dInfo(s).name, old, new);     % Rename session
                 OldFolder = fullfile(dInfo(s).folder, dInfo(s).name);                   % Name of old folder
                 NewFolder = fullfile(dInfo(s).folder, dInfo(s).newname);                % Name of new folder
                 movefile(OldFolder, NewFolder)                                          % Rename folder
