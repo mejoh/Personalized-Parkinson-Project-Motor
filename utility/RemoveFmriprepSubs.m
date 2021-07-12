dfe8b4b356776fbae39f8631ad2407dccc0e9160
Martin Johansson <m.johansson@donders.ru.nl>
2021-07-12 16:37:36 +0200

    Create RemoveFmriprepSubs.m

---
+++ utility/RemoveFmriprepSubs.m
@@ -0,0 +1,40 @@
+% Specify directories
+bidsdir='/project/3022026.01/pep/bids';
+fmriprepdir='/project/3022026.01/pep/bids/derivatives/fmriprep';
+sub = cellstr(spm_select('List', bidsdir, 'dir', '^sub-POMU.*'));
+
+% Skip subjects without htmlfile
+Sel=true(length(sub),1);
+for n=1:length(sub)
+    htmlfile = spm_select('FPList', fmriprepdir, ['^' sub{n} '.html']);
+    if ~exist(htmlfile, 'file')
+        Sel(n)=false;
+    end
+end
+sub=sub(Sel);
+
+s1 = 'ses-PITVisit1';
+s2 = 'ses-PITVisit2';
+
+Sel=false(length(sub),1);
+for n=1:length(sub)
+    
+    session1 = spm_select('FPList', fullfile(fmriprepdir, sub{n}), 'dir', s1);
+    session2 = spm_select('FPList', fullfile(fmriprepdir, sub{n}), 'dir', s2);
+    
+    if exist(session1, 'dir') && exist(session2, 'dir')
+        Sel(n) = true;
+    end
+    
+end
+sub = sub(Sel);
+
+for n=1:length(sub)
+    
+    htmlfile = spm_select('FPList', fmriprepdir, ['^' sub{n} '.html']);
+    delete(htmlfile);
+    
+    subprepdir = spm_select('FPList', fmriprepdir, 'dir', sub{n});
+    rmdir(subprepdir, 's');
+    
+end
