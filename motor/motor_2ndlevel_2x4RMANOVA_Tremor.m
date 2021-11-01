da0a3897ab72f4f0ab2e25520ff6be67ae367b82
Martin Johansson <m.johansson@donders.ru.nl>
2021-11-01 16:11:02 +0100

    Update motor_2ndlevel_2x4RMANOVA_Tremor.m
     - Added selection of groups to compare
     - Outlier analysis
     - Altered names
     - Updated classification files

--- motor/motor_2ndlevel_2x4RMANOVA_Tremor.m
+++ motor/motor_2ndlevel_2x4RMANOVA_Tremor.m
@@ -7,15 +7,16 @@
 % 1. 'TremorRegressors'
 % 2. 'ClinVars'
 % Classification takes on one of 4 values:
-% 1 = Healthy, 2 = Tremor<1, 3 = Tremor>=2, 4 = Tremor=1
+% For TremorRegressors option: 1 = Healthy, 2 = No tremor, 3 = Tremor, >2Hz, <8Hz, 4 = Unclear
+% For ClinVars opttion: 1 = Healthy, 2 = Tremor<1, 3 = Tremor>=2, 4 = Tremor=1
 % Tremor is considered in the on-state, on the 'study watch side', whatever that means...
 
 function motor_2ndlevel_2x4RMANOVA_Tremor(Comparison, TremorClassification, exclude_outliers)
 
 if nargin<1
     Comparison = 'TremorNonTremor';
-    TremorClassification = 'ClinVars';
-    exclude_outliers = false;
+    TremorClassification = 'TremorRegressors';
+    exclude_outliers = true;
 end
 
 %% Group to comapre against controls
@@ -28,8 +29,10 @@ spm('defaults', 'FMRI');
 %% Directories
 
 ses = 'ses-Visit1';
-ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_NoTrem';
-Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, 'Group', 'con_0001', ses), '.*sub-POM.*'));
+GroupFolder = 'Group';
+ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem';
+ClinicalConfs = readtable('/project/3022026.01/pep/ClinVars/derivatives/database_clinical_confounds_fmri_2021-08-25.csv');
+Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, GroupFolder, 'con_0001', ses), '.*sub-POM.*'));
 Sub = extractBetween(Sub, 1, 31);
 fprintf('Number of subjects processed: %i\n', numel(Sub))
 
@@ -43,8 +46,23 @@ for n = 1:numel(Sub)
 end
 Sub = Sub(Sel);
 
+% This little piece allows you to split the analysis based on some pattern.
+% Good for testing whether there are undeteced outliers in your data.
+RemovalPattern = '';
+Sub = remove_subs(Sub,RemovalPattern);
+
 SubInfo.Sub = extractBetween(Sub, 8, 31);
 SubInfo.Group = extractBetween(Sub, 1, 6);
+
+Sel = false(size(Sub));
+for n = 1:height(ClinicalConfs)
+    if (strcmp(Comparison, 'NonTremorHc') || strcmp(Comparison, 'TremorHc')) && (strcmp(ClinicalConfs.Group(n), 'PD_POM') || strcmp(ClinicalConfs.Group(n), 'HC_PIT'))
+        Sel(n) = true;
+    elseif strcmp(Comparison, 'TremorNonTremor') && strcmp(ClinicalConfs.Group(n), 'PD_POM')
+        Sel(n) = true;
+    end
+end
+ClinicalConfs = ClinicalConfs(Sel,:);
 
 if strcmp(TremorClassification, 'TremorRegressors')
     SubInfo = TremorClassificationByRegressors(SubInfo);
@@ -55,13 +73,13 @@ else
     error(msg)
 end
 
-fSubInfo = char(fullfile(ANALYSESDir, 'Group', 'SubInfo.csv'));
+fSubInfo = char(fullfile(ANALYSESDir, GroupFolder, 'SubInfo.csv'));
 tSubInfo = struct2table(SubInfo);
 writetable(tSubInfo, fSubInfo);
     
 
 Sel = false(size(SubInfo.Sub));
-for n = 1:numel(Sub)
+for n = 1:numel(SubInfo.Sub)
     if strcmp(Comparison, 'TremorHc')
         if SubInfo.Selection(n) == 3 || SubInfo.Selection(n) == 1
             Sel(n) = true;
@@ -81,17 +99,29 @@ SubInfo.Group = SubInfo.Group(Sel);
 SubInfo.Selection = SubInfo.Selection(Sel);
 
 % Exclude outliers
+mriqc_outliers = readtable('/project/3024006.02/Analyses/mriqc_outliers.txt');
+Con1 = '/project/3024006.02/Analyses/QC/1st_level/con_0001/Group.txt';
+Con2 = '/project/3024006.02/Analyses/QC/1st_level/con_0002/Group.txt';
+Con3 = '/project/3024006.02/Analyses/QC/1st_level/con_0003/Group.txt';
+ResMS = '/project/3024006.02/Analyses/QC/1st_level/ResMS/Group.txt';
+Con1_f = readtable(Con1);
+Con1_f_s = Con1_f(Con1_f.Outlier==1,:);
+Con2_f = readtable(Con2);
+Con2_f_s = Con2_f(Con2_f.Outlier==1,:);
+Con3_f = readtable(Con3);
+Con3_f_s = Con3_f(Con3_f.Outlier==1,:);
+ResMS_f = readtable(ResMS);
+ResMS_f_s = ResMS_f(ResMS_f.Outlier==1,:);
+outliers = unique([Con1_f_s.Sub; Con2_f_s.Sub; Con3_f_s.Sub; ResMS_f_s.Sub; mriqc_outliers]);
 if istrue(exclude_outliers)
-    outliers = ["sub-POMU06C08BB18FE38A3B" "sub-POMU10D1CBD2A4EB7831" "sub-POMU1EC01A53575D7104" "sub-POMU242851EF4B22A600" "sub-POMU7AABE759AC531D35" "sub-POMU80C7D6A96AA13388" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0A6FB4D3AF0D3B5" "sub-POMUB24789880E112F8F" "sub-POMUD2870200E030B451" "sub-POMUE49FF489B8076A3E" "sub-POMU06664E2F91AA04E0" "sub-POMU10D1CBD2A4EB7831" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0B32692E1393605" "sub-POMUC80F15E9B41B3EE4" "sub-POMU10D1CBD2A4EB7831" "sub-POMU7AABE759AC531D35" "sub-POMU907470A979431AAF" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0B32692E1393605" "sub-POMUC80F15E9B41B3EE4" "sub-POMU020A9277DF9F5A83" "sub-POMU02C38D3DE0820685" "sub-POMU0E19B895DF700AB0" "sub-POMU12FA62414399DD8F" "sub-POMU1C7AEA3B0ADEB876" "sub-POMU1E7EF007D32758D4" "sub-POMU1E7EF007D32758D4" "sub-POMU1EDD7C2E9E8DF70C" "sub-POMU3227DABC7764ADB0" "sub-POMU4A7412A72D2973F1" "sub-POMU54503B880C9EB267" "sub-POMU566ED54BF23566B6" "sub-POMU58B6CDD53AD4049C" "sub-POMU7E16E08D6D3BD46C" "sub-POMU82E58ECC13773EA2" "sub-POMU86B947749A70E997" "sub-POMUA6F5671F320EA927" "sub-POMUAC2513F0E5E32349" "sub-POMUBA95A9F6A41E872C" "sub-POMUDEC90EA2FF4B337C" "sub-POMUE0EFDE0DD571E3A5" "sub-POMUEE16EA86913A5BEC"];
     Sel = true(size(SubInfo.Sub));
     for n = 1:numel(SubInfo.Sub)
-        if contains(SubInfo.Sub{n}, unique(outliers))
+        if contains(SubInfo.Sub{n}, outliers)
            Sel(n) = false;
         fprintf('Excluding outlier: %s %s \n', SubInfo.Sub{n}, SubInfo.Group{n})
         end
     end
-    SubInfo.Sub = SubInfo.Sub(Sel);
-    SubInfo.Group = SubInfo.Group(Sel);
+    SubInfo = subset_subinfo(SubInfo,Sel);
 end
 
 %% Collect events.json and confound files
@@ -99,13 +129,13 @@ end
 SubInfo.ConfFiles = cell(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
     if contains(SubInfo.Group{n}, '_PIT')
-        Session = 'ses-Visit1_PIT';
+        Session = 'ses-PITVisit1';
     else
-        Session = 'ses-Visit1';
+        Session = 'ses-POMVisit1';
     end
-    SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_regressors3.mat$');
+    SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_timeseries3.mat$');
     if isempty(SubInfo.ConfFiles{n})
-        SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_regressors2.mat$');
+        SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, Session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_timeseries2.mat$');
     end
 end
 
@@ -117,6 +147,36 @@ for n = 1:numel(SubInfo.Sub)
     FrameDisp = Confounds.R(:,strcmp(Confounds.names, 'framewise_displacement'));
     FrameDisp(isnan(FrameDisp)) = 0;
     SubInfo.FD(n) = mean(FrameDisp);
+end
+
+%% Age and gender
+% Exclude subjects with missing Age and Gender
+Sel = true(size(SubInfo.Sub));
+SubInfo.Age = zeros(size(SubInfo.Sub));
+SubInfo.Gender = cell(size(SubInfo.Sub));
+for n = 1:numel(SubInfo.Sub)
+    
+    subid = find(contains(ClinicalConfs.pseudonym, SubInfo.Sub{n}));
+    
+    if isempty(subid) || isnan(ClinicalConfs.Age(subid)) || strcmp(ClinicalConfs.Gender(subid), 'NA')
+        fprintf('Missing values, excluding %s...\n', SubInfo.Sub{n})
+        Sel(n) = false;
+    else
+        SubInfo.Age(n) = ClinicalConfs.Age(subid);
+        SubInfo.Gender{n} = ClinicalConfs.Gender(subid);
+    end
+    
+end
+fprintf('%i subjects have missing Age/Gender, excluding...\n', length(Sel) - sum(Sel))
+SubInfo = subset_subinfo(SubInfo, Sel);
+
+SubInfo.Gender_num = zeros(size(SubInfo.Gender));
+for n = 1:numel(SubInfo.Gender)
+    if strcmp(SubInfo.Gender{n}, 'Male')
+        SubInfo.Gender_num(n) = 0;
+    else
+        SubInfo.Gender_num(n) = 1;
+    end
 end
 
 %% Examine correlation structure between relevant regressors
@@ -143,31 +203,35 @@ SubInfo.con_0003 = cell(size(SubInfo.Sub));
 SubInfo.con_0004 = cell(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
     if strcmp(SubInfo.Group{n}, 'PD_POM')
-        SubInfo.con_0001{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{1}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{1} '.*.nii']);
-        SubInfo.con_0002{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{2}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{2} '.*.nii']);
-        SubInfo.con_0003{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{3}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{3} '.*.nii']);
-        SubInfo.con_0004{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{4}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{4} '.*.nii']);
+        SubInfo.con_0001{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{1}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{1} '.*.nii']);
+        SubInfo.con_0002{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{2}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{2} '.*.nii']);
+        SubInfo.con_0003{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{3}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{3} '.*.nii']);
+        SubInfo.con_0004{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{4}, 'ses-Visit1'), ['PD_POM.*' SubInfo.Sub{n} '.*' ConList{4} '.*.nii']);
     elseif strcmp(SubInfo.Group{n}, 'HC_PIT')
-        SubInfo.con_0001{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{1}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{1} '.*.nii']);
-        SubInfo.con_0002{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{2}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{2} '.*.nii']);
-        SubInfo.con_0003{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{3}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{3} '.*.nii']);
-        SubInfo.con_0004{n} = spm_select('FPList', fullfile(ANALYSESDir, 'Group', ConList{4}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{4} '.*.nii']);
+        SubInfo.con_0001{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{1}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{1} '.*.nii']);
+        SubInfo.con_0002{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{2}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{2} '.*.nii']);
+        SubInfo.con_0003{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{3}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{3} '.*.nii']);
+        SubInfo.con_0004{n} = spm_select('FPList', fullfile(ANALYSESDir, GroupFolder, ConList{4}, 'ses-Visit1'), ['HC_PIT.*' SubInfo.Sub{n} '.*' ConList{4} '.*.nii']);
     end
 end
 
 Inputs = cell(10,1);
 if strcmp(Comparison, 'TremorHc')
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcTremor x ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'TremorHc_x_ExtInt2Int3Catch')};
     G1 = 1;
     G2 = 3;
 elseif strcmp(Comparison, 'NonTremorHc')
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcNonTremor x ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'NonTremorHc_x_ExtInt2Int3Catch')};
     G1 = 1;
     G2 = 2;
 elseif strcmp(Comparison, 'TremorNonTremor')
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'TremorNonTremor x ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'TremorNonTremor_x_ExtInt2Int3Catch')};
     G1 = 2;
     G2 = 3;
+end
+
+if exclude_outliers
+    Inputs{1,1} = {[char(Inputs{1,1}) '_NoOutliers']};
 end
 
 Inputs{2,1} = SubInfo.con_0001(SubInfo.Selection == G1,:);
@@ -180,7 +244,13 @@ Inputs{7,1} = SubInfo.con_0003(SubInfo.Selection == G2,:);
 Inputs{9,1} = SubInfo.con_0004(SubInfo.Selection == G2,:);
 G1_FD = SubInfo.FD(SubInfo.Selection == G1,:);
 G2_FD = SubInfo.FD(SubInfo.Selection == G2,:);
+G1_Age = SubInfo.Age(SubInfo.Selection == G1,:);
+G2_Age = SubInfo.Age(SubInfo.Selection == G2,:);
+G1_Gender = SubInfo.Gender_num(SubInfo.Selection == G1,:);
+G2_Gender = SubInfo.Gender_num(SubInfo.Selection == G2,:);
 Inputs{10,1} = [G1_FD; G1_FD; G1_FD; G1_FD; G2_FD; G2_FD; G2_FD; G2_FD];
+Inputs{11,1} = [G1_Age; G1_Age; G1_Age; G1_Age; G2_Age; G2_Age; G2_Age; G2_Age];
+Inputs{12,1} = [G1_Gender; G1_Gender; G1_Gender; G1_Gender; G2_Gender; G2_Gender; G2_Gender; G2_Gender];
 
 %% Run
 
@@ -196,8 +266,8 @@ end
 
 function [SubInfo] = TremorClassificationByRegressors(SubInfo)
 
-TremorClass = load('/project/3024006.02/Analyses/EMG/motor/manually_checked/Martin/Tremor_check-09-Nov-2020.mat');
-PeakClass = load('/project/3024006.02/Analyses/EMG/motor/manually_checked/Martin/Peak_check-09-Nov-2020.mat');
+TremorClass = load('/project/3024006.02/Analyses/EMG/motor/manually_checked/Martin/Tremor_check-24-Mar-2021.mat');
+PeakClass = load('/project/3024006.02/Analyses/EMG/motor/manually_checked/Martin/Peak_check-24-Mar-2021.mat');
 TremorRegFolder = fullfile('/project/3024006.02/Analyses/EMG/motor/processing/prepemg/Regressors/ZSCORED');
 
 Sel = true(size(SubInfo.Sub));
@@ -210,8 +280,7 @@ for n = 1:numel(SubInfo.Sub)
         end
     end
 end
-SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Group = SubInfo.Group(Sel);
+SubInfo = subset_subinfo(SubInfo,Sel);
 SubInfo.Selection = zeros(numel(SubInfo.Sub),1);
 
 for n = 1:numel(SubInfo.Sub)
