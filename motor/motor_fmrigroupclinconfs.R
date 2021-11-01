beab10b4fc7af879de56082206a7e151556c4bf4
Martin Johansson <m.johansson@donders.ru.nl>
2021-11-01 16:14:25 +0100

    Update motor_fmrigroupclinconfs.R

--- motor/motor_fmrigroupclinconfs.R
+++ motor/motor_fmrigroupclinconfs.R
@@ -1,15 +1,18 @@
-df.clin.pom <- read_csv('P:/3022026.01/pep/ClinVars/derivatives/database_clinical_variables_2021-04-15.csv')
-df.clin.pit <- read_csv('P:/3022026.01/pep/ClinVars/derivatives/database_PIT_clinical_variables_2021-04-15.csv')
+df.clin.pom <- read_csv('P:/3022026.01/pep/ClinVars/derivatives/database_clinical_variables_2021-08-25.csv')
+df.clin.pit <- read_csv('P:/3022026.01/pep/ClinVars/derivatives/database_PIT_clinical_variables_2021-08-25.csv')
 
 df.clin.pom2 <- df.clin.pom %>%
         filter(Timepoint == 'ses-POMVisit1') %>%
-        select(pseudonym, Age, Gender, Up3OfTotal, Timepoint, Group)
+        select(pseudonym, Age, Gender, Up3OfTotal, Timepoint, Group) %>%
+        mutate(Gender = as.factor(Gender))
+levels(df.clin.pom2$Gender)  <- c('Male', 'Female')
 df.clin.pit2 <- df.clin.pit %>%
         filter(Timepoint == 'ses-PITVisit1') %>%
         select(pseudonym, Age, Gender, Up3OfTotal, Timepoint, Group)
 
 df <- full_join(df.clin.pom2, df.clin.pit2) %>%
-        arrange(pseudonym, Timepoint, Group)
+        arrange(pseudonym, Timepoint, Group) %>%
+        filter(!is.na(Group))
 
-outputname <- 'P:/3022026.01/pep/ClinVars/derivatives/database_clinical_confounds_fmri.csv'
+outputname <- paste('P:/3022026.01/pep/ClinVars/derivatives/database_clinical_confounds_fmri_', today(), '.csv', sep='')
 write_csv(df, outputname)
