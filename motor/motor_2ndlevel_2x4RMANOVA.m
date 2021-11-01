9046c1661a8b7b2d4299ce7bf9d3ebc8b22defd9
Martin Johansson <m.johansson@donders.ru.nl>
2021-11-01 16:09:41 +0100

    Update motor_2ndlevel_2x4RMANOVA.m
     - Altered outlier removal
     - Added interpolation for age and gender (should really be imputed, implement this later)
     - Altered naming convention of output folders

--- motor/motor_2ndlevel_2x4RMANOVA.m
+++ motor/motor_2ndlevel_2x4RMANOVA.m
@@ -1,4 +1,5 @@
 % Contrast one group against another
+% Run with matlab/R2020b
 
 function motor_2ndlevel_2x4RMANOVA(Offstate, exclude_outliers)
 
@@ -6,6 +7,7 @@ function motor_2ndlevel_2x4RMANOVA(Offstate, exclude_outliers)
 
 if nargin<1 || isempty(Offstate)
     Offstate = false;
+    exclude_outliers = false;
 end
 
 %% Paths
@@ -16,10 +18,10 @@ spm('defaults', 'FMRI');
 %% Directories
 
 ses = 'ses-Visit1';
+GroupFolder = 'Group';
 ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem';
-ClinicalConfs = readtable('/project/3022026.01/pep/ClinVars/derivatives/database_clinical_confounds_fmri.csv');
-% ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_NoPMOD_TimeDer_BPCtrl';
-Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, 'Group', 'con_0001', ses), '.*sub-POM.*'));
+ClinicalConfs = readtable('/project/3022026.01/pep/ClinVars/derivatives/database_clinical_confounds_fmri_2021-08-25.csv');
+Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, GroupFolder, 'con_0001', ses), '.*sub-POM.*'));
 Sub = extractBetween(Sub, 1, 31);
 fprintf('Number of subjects processed: %i\n', numel(Sub))
 
@@ -49,17 +51,30 @@ end
 ClinicalConfs = ClinicalConfs(Sel,:);
 
 % Exclude outliers
+mriqc_outliers = readtable('/project/3024006.02/Analyses/mriqc_outliers.txt');
+Con1 = '/project/3024006.02/Analyses/QC/1st_level/con_0001/Group.txt';
+Con2 = '/project/3024006.02/Analyses/QC/1st_level/con_0002/Group.txt';
+Con3 = '/project/3024006.02/Analyses/QC/1st_level/con_0003/Group.txt';
+ResMS = '/project/3024006.02/Analyses/QC/1st_level/ResMS/Group.txt';
+Con1_f = readtable(Con1);
+Con1_f_s = Con1_f(Con1_f.Outlier==1,:);
+Con2_f = readtable(Con2);
+Con2_f_s = Con2_f(Con2_f.Outlier==1,:);
+Con3_f = readtable(Con3);
+Con3_f_s = Con3_f(Con3_f.Outlier==1,:);
+ResMS_f = readtable(ResMS);
+ResMS_f_s = ResMS_f(ResMS_f.Outlier==1,:);
+outliers = unique([Con1_f_s.Sub; Con2_f_s.Sub; Con3_f_s.Sub; ResMS_f_s.Sub; mriqc_outliers]);
 if istrue(exclude_outliers)
-    outliers = ["sub-POMU06C08BB18FE38A3B" "sub-POMU10D1CBD2A4EB7831" "sub-POMU1EC01A53575D7104" "sub-POMU242851EF4B22A600" "sub-POMU7AABE759AC531D35" "sub-POMU80C7D6A96AA13388" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0A6FB4D3AF0D3B5" "sub-POMUB24789880E112F8F" "sub-POMUD2870200E030B451" "sub-POMUE49FF489B8076A3E" "sub-POMU06664E2F91AA04E0" "sub-POMU10D1CBD2A4EB7831" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0B32692E1393605" "sub-POMUC80F15E9B41B3EE4" "sub-POMU10D1CBD2A4EB7831" "sub-POMU7AABE759AC531D35" "sub-POMU907470A979431AAF" "sub-POMUA6BD4DCB8040A67B" "sub-POMUB0B32692E1393605" "sub-POMUC80F15E9B41B3EE4" "sub-POMU020A9277DF9F5A83" "sub-POMU02C38D3DE0820685" "sub-POMU0E19B895DF700AB0" "sub-POMU12FA62414399DD8F" "sub-POMU1C7AEA3B0ADEB876" "sub-POMU1E7EF007D32758D4" "sub-POMU1E7EF007D32758D4" "sub-POMU1EDD7C2E9E8DF70C" "sub-POMU3227DABC7764ADB0" "sub-POMU4A7412A72D2973F1" "sub-POMU54503B880C9EB267" "sub-POMU566ED54BF23566B6" "sub-POMU58B6CDD53AD4049C" "sub-POMU7E16E08D6D3BD46C" "sub-POMU82E58ECC13773EA2" "sub-POMU86B947749A70E997" "sub-POMUA6F5671F320EA927" "sub-POMUAC2513F0E5E32349" "sub-POMUBA95A9F6A41E872C" "sub-POMUDEC90EA2FF4B337C" "sub-POMUE0EFDE0DD571E3A5" "sub-POMUEE16EA86913A5BEC"];
     Sel = true(size(SubInfo.Sub));
     for n = 1:numel(SubInfo.Sub)
-        if contains(SubInfo.Sub{n}, unique(outliers))
+        if contains(SubInfo.Sub{n}, outliers)
            Sel(n) = false;
         fprintf('Excluding outlier: %s %s \n', SubInfo.Sub{n}, SubInfo.Group{n})
         end
     end
-    SubInfo.Sub = SubInfo.Sub(Sel);
-    SubInfo.Group = SubInfo.Group(Sel);
+    fprintf('%i outliers have been excluded \n', length(Sel) - sum(Sel))
+    SubInfo = subset_subinfo(SubInfo, Sel);
 end
 
 %% Collect events.json and confound files
@@ -89,6 +104,27 @@ end
 
 %% Age and Gender
 
+% Interpolate age and gender
+% SubInfo.Age = zeros(size(SubInfo.Sub));
+% SubInfo.Gender = cell(size(SubInfo.Sub));
+% for n = 1:numel(SubInfo.Sub)
+%     
+%     subid = find(contains(ClinicalConfs.pseudonym, SubInfo.Sub{n}));
+%     
+%     if isempty(subid) || isnan(ClinicalConfs.Age(subid)) || strcmp(ClinicalConfs.Gender(subid), 'NA')
+%         fprintf('Missing values, interpolating...\n')
+%         SubInfo.Age(n) = round(mean(ClinicalConfs.Age, 'omitnan'));
+%         SubInfo.Gender{n} = cellstr('Male');
+%     else
+%         SubInfo.Age(n) = ClinicalConfs.Age(subid);
+%         SubInfo.Gender{n} = ClinicalConfs.Gender(subid);
+%     end
+%     
+% end
+% 
+
+% Exclude subjects with missing Age and Gender
+Sel = true(size(SubInfo.Sub));
 SubInfo.Age = zeros(size(SubInfo.Sub));
 SubInfo.Gender = cell(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
@@ -96,15 +132,16 @@ for n = 1:numel(SubInfo.Sub)
     subid = find(contains(ClinicalConfs.pseudonym, SubInfo.Sub{n}));
     
     if isempty(subid) || isnan(ClinicalConfs.Age(subid)) || strcmp(ClinicalConfs.Gender(subid), 'NA')
-        fprintf('Missing values, interpolating...\n')
-        SubInfo.Age(n) = round(mean(ClinicalConfs.Age, 'omitnan'));
-        SubInfo.Gender{n} = cellstr('Male');
+        fprintf('Missing values, excluding %s...\n', SubInfo.Sub{n})
+        Sel(n) = false;
     else
         SubInfo.Age(n) = ClinicalConfs.Age(subid);
         SubInfo.Gender{n} = ClinicalConfs.Gender(subid);
     end
     
 end
+fprintf('%i subjects have missing Age/Gender, excluding...\n', length(Sel) - sum(Sel))
+SubInfo = subset_subinfo(SubInfo, Sel);
 
 SubInfo.Gender_num = zeros(size(SubInfo.Gender));
 for n = 1:numel(SubInfo.Gender)
@@ -135,59 +172,59 @@ end
 ConList = {'con_0001' 'con_0002' 'con_0003' 'con_0004'};
 Inputs = cell(10,1);
 if Offstate
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOff_ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOff_x_ExtInt2Int3Catch')};
 else
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOn_ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOn_x_ExtInt2Int3Catch')};
 end
 
 if ~istrue(exclude_outliers) && istrue(Offstate)
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOff_ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOff_x_ExtInt2Int3Catch')};
 elseif ~istrue(exclude_outliers) && ~istrue(Offstate)
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOn_ExtInt2Int3Catch')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOn_x_ExtInt2Int3Catch')};
 elseif istrue(exclude_outliers) && istrue(Offstate)
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOff_ExtInt2Int3Catch_NoOutliers')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOff_x_ExtInt2Int3Catch_NoOutliers')};
 elseif istrue(exclude_outliers) && ~istrue(Offstate)
-    Inputs{1,1} = {fullfile(ANALYSESDir, 'Group', 'HcOn_ExtInt2Int3Catch_NoOutliers')};
+    Inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, 'HcOn_x_ExtInt2Int3Catch_NoOutliers')};
 end
 
-ExtHc = dir(fullfile(ANALYSESDir, 'Group', ConList{1}, ses, 'HC*'));
+ExtHc = dir(fullfile(ANALYSESDir, GroupFolder, ConList{1}, ses, 'HC*'));
 ExtHc_files = {ExtHc.name}';
 ExtHc_files = ExtHc_files(contains({ExtHc.name}, SubInfo.Sub));
-Inputs{2,1} = fullfile(ANALYSESDir, 'Group', ConList{1}, ses, ExtHc_files);
-Int2Hc = dir(fullfile(ANALYSESDir, 'Group', ConList{2}, ses, 'HC*'));
+Inputs{2,1} = fullfile(ANALYSESDir, GroupFolder, ConList{1}, ses, ExtHc_files);
+Int2Hc = dir(fullfile(ANALYSESDir, GroupFolder, ConList{2}, ses, 'HC*'));
 Int2Hc_files = {Int2Hc.name}';
 Int2Hc_files = Int2Hc_files(contains({Int2Hc.name}, SubInfo.Sub));
-Inputs{4,1} = fullfile(ANALYSESDir, 'Group', ConList{2}, ses, Int2Hc_files);
-Int3Hc = dir(fullfile(ANALYSESDir, 'Group', ConList{3}, ses, 'HC*'));
+Inputs{4,1} = fullfile(ANALYSESDir, GroupFolder, ConList{2}, ses, Int2Hc_files);
+Int3Hc = dir(fullfile(ANALYSESDir, GroupFolder, ConList{3}, ses, 'HC*'));
 Int3Hc_files = {Int3Hc.name}';
 Int3Hc_files = Int3Hc_files(contains({Int3Hc.name}, SubInfo.Sub));
-Inputs{6,1} = fullfile(ANALYSESDir, 'Group', ConList{3}, ses, Int3Hc_files);
-CatchHc = dir(fullfile(ANALYSESDir, 'Group', ConList{4}, ses, 'HC*'));
+Inputs{6,1} = fullfile(ANALYSESDir, GroupFolder, ConList{3}, ses, Int3Hc_files);
+CatchHc = dir(fullfile(ANALYSESDir, GroupFolder, ConList{4}, ses, 'HC*'));
 CatchHc_files = {CatchHc.name}';
 CatchHc_files = CatchHc_files(contains({CatchHc.name}, SubInfo.Sub));
-Inputs{8,1} = fullfile(ANALYSESDir, 'Group', ConList{4}, ses, CatchHc_files);
+Inputs{8,1} = fullfile(ANALYSESDir, GroupFolder, ConList{4}, ses, CatchHc_files);
 
 if Offstate
     Pd = 'PD_PIT*';
 else
     Pd = 'PD_POM*';
 end
-ExtPd = dir(fullfile(ANALYSESDir, 'Group', ConList{1}, ses, Pd));
+ExtPd = dir(fullfile(ANALYSESDir, GroupFolder, ConList{1}, ses, Pd));
 ExtPd_files = {ExtPd.name}';
 ExtPd_files = ExtPd_files(contains({ExtPd.name}, SubInfo.Sub));
-Inputs{3,1} = fullfile(ANALYSESDir, 'Group', ConList{1}, ses, ExtPd_files);
-Int2Pd = dir(fullfile(ANALYSESDir, 'Group', ConList{2}, ses, Pd));
+Inputs{3,1} = fullfile(ANALYSESDir, GroupFolder, ConList{1}, ses, ExtPd_files);
+Int2Pd = dir(fullfile(ANALYSESDir, GroupFolder, ConList{2}, ses, Pd));
 Int2Pd_files = {Int2Pd.name}';
 Int2Pd_files = Int2Pd_files(contains({Int2Pd.name}, SubInfo.Sub));
-Inputs{5,1} = fullfile(ANALYSESDir, 'Group', ConList{2}, ses, Int2Pd_files);
-Int3Pd = dir(fullfile(ANALYSESDir, 'Group', ConList{3}, ses, Pd));
+Inputs{5,1} = fullfile(ANALYSESDir, GroupFolder, ConList{2}, ses, Int2Pd_files);
+Int3Pd = dir(fullfile(ANALYSESDir, GroupFolder, ConList{3}, ses, Pd));
 Int3Pd_files = {Int3Pd.name}';
 Int3Pd_files = Int3Pd_files(contains({Int3Pd.name}, SubInfo.Sub));
-Inputs{7,1} = fullfile(ANALYSESDir, 'Group', ConList{3}, ses, Int3Pd_files);
-CatchPd = dir(fullfile(ANALYSESDir, 'Group', ConList{4}, ses, Pd));
+Inputs{7,1} = fullfile(ANALYSESDir, GroupFolder, ConList{3}, ses, Int3Pd_files);
+CatchPd = dir(fullfile(ANALYSESDir, GroupFolder, ConList{4}, ses, Pd));
 CatchPd_files = {CatchPd.name}';
 CatchPd_files = CatchPd_files(contains({CatchPd.name}, SubInfo.Sub));
-Inputs{9,1} = fullfile(ANALYSESDir, 'Group', ConList{4}, ses, CatchPd_files);
+Inputs{9,1} = fullfile(ANALYSESDir, GroupFolder, ConList{4}, ses, CatchPd_files);
 
 FD_hc = SubInfo.FD(strcmp(SubInfo.Group, 'HC_PIT'));
 Age_hc = SubInfo.Age(strcmp(SubInfo.Group, 'HC_PIT'));
