f3267c6c5fe232597c07ac59160b2d622fe0d12f
Martin Johansson <m.johansson@donders.ru.nl>
2021-11-01 16:13:37 +0100

    Updated brain-behavior correlation scripts

--- motor/motor_2ndlevel_OneSampleTtests.m
+++ motor/motor_2ndlevel_OneSampleTtests.m
@@ -1,15 +1,21 @@
 % NOTE: Doesnt work with offstate at the moment since no castor data is available
 
-function motor_2ndlevel_OneSampleTtests(BaselineOnly, Subscore)
+function motor_2ndlevel_OneSampleTtests(BaselineOnly, Subscore, exclude_outliers, Subtype)
 % Subscores
-% Total BradySum RigiditySum RestTremAmpSum 
-% PIGDSum ActionTremorSum CompositeTremorSum TotalOnOffDelta BradySumOnOffDelta RestTremAmpSumOnOffDelta
+% Total AppendicularSum CompositeTremorSum
+% PIGDSum ActionTremorSum TotalOnOffDelta BradySumOnOffDelta RestTremAmpSumOnOffDelta RestTremAmpSum BradySum RigiditySum 
 
 if nargin < 1 || isempty(BaselineOnly)
     BaselineOnly = false;
 end
 if nargin < 1 || isempty(Subscore)
     Subscore = 'Total';
+end
+if nargin < 1 || isempty(exclude_outliers)
+    exclude_outliers = true;
+end
+if nargin < 1 || isempty(Subtype)
+    Subtype = [];
 end
 
 %% Paths
@@ -21,15 +27,27 @@ spm('defaults', 'FMRI');
 %% Directories
 
 ses = 'ses-POMVisit1';
+GroupFolder = 'Group';
 ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem';
-ClinicalDataFile = '/project/3022026.01/pep/ClinVars/derivatives/database_clinical_variables_2021-04-15.csv';
-Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, 'Group', 'con_0001', 'ses-Visit1'), '.*sub-POM.*'));
+ClinicalDataFile = '/project/3022026.01/pep/ClinVars/derivatives/database_clinical_variables_2021-08-30.csv';
+SubtypeDataFile = '/project/3022026.01/pep/deprecated_ClinVars/derivatives/Subtypes_2021-04-12.csv';
+Sub = cellstr(spm_select('List', fullfile(ANALYSESDir, GroupFolder, 'con_0001', 'ses-Visit1'), '.*sub-POM.*'));
 Sub = extractBetween(Sub, 1, 31);
 fprintf('Number of subjects found: %i\n', numel(Sub))
 
 %% Import clinical data
 
 ClinVars = readtable(ClinicalDataFile);
+ClinVars = ClinVars(contains(ClinVars.Timepoint, extractAfter(ses, 'POM')),:);
+
+% Subtype = 'Mild-Motor';
+if ~isempty(Subtype)
+    Classification = readtable(SubtypeDataFile);
+    Classification = Classification(strcmp(Classification.Subtype, Subtype),:);
+    Sub = Sub(contains(Sub,Classification.pseudonym));
+    ClinVars = ClinVars(contains(ClinVars.pseudonym, Classification.pseudonym),:);
+    fprintf('Analyzing subtype: %s, n = %i \n', Subtype, numel(Sub))
+end
 
 % Subset data frame
 rID_task = strcmp(ClinVars.MriNeuroPsychTask, 'Motor');
@@ -49,7 +67,7 @@ fprintf('Number of Motor subjects with multiple sessions found in clinical data:
 
 %% Framewise displacement
 SubInfo.Sub = Sub(contains(Sub,'PD_POM'));
-SubInfo.ConfFiles = cell(size(Sub));
+SubInfo.ConfFiles = cell(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
     s = extractAfter(SubInfo.Sub{n}, 7);
     SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, s, 'ses-POMVisit1'), '^.*task-motor_acq-MB6_run-.*_desc-confounds_timeseries3.mat$');
@@ -70,35 +88,63 @@ for n = 1:numel(SubInfo.Sub)
     end
 end
 
+% Exclude outliers
+mriqc_outliers = readtable('/project/3024006.02/Analyses/mriqc_outliers.txt');
+Con1 = '/project/3024006.02/Analyses/QC/1st_level/con_0001/Group.txt';
+Con2 = '/project/3024006.02/Analyses/QC/1st_level/con_0002/Group.txt';
+Con3 = '/project/3024006.02/Analyses/QC/1st_level/con_0003/Group.txt';
+ResMS = '/project/3024006.02/Analyses/QC/1st_level/ResMS/Group.txt';
+Con1_f = readtable(Con1);
+Con1_f_s = Con1_f(Con1_f.Outlier==1,:);
+Con2_f = readtable(Con2);
+Con2_f_s = Con2_f(Con2_f.Outlier==1,:);
+Con3_f = readtable(Con3);
+Con3_f_s = Con3_f(Con3_f.Outlier==1,:);
+ResMS_f = readtable(ResMS);
+ResMS_f_s = ResMS_f(ResMS_f.Outlier==1,:);
+outliers = unique([Con1_f_s.Sub; Con2_f_s.Sub; Con3_f_s.Sub; ResMS_f_s.Sub; mriqc_outliers]);
+if istrue(exclude_outliers)
+    Sel = true(size(SubInfo.Sub));
+    for n = 1:numel(SubInfo.Sub)
+        if contains(SubInfo.Sub{n}, outliers)
+           Sel(n) = false;
+        fprintf('Excluding outlier: %s \n', SubInfo.Sub{n})
+        end
+    end
+    SubInfo = subset_subinfo(SubInfo,Sel);
+    rID = contains(ClinVars_subset.pseudonym, outliers);
+    ClinVars_subset = ClinVars_subset(~rID,:);
+end
+
 %% Assemble inputs
 %ConList = {'con_0001' 'con_0002' 'con_0003' 'con_0004' 'con_0005' 'con_0006' 'con_0007' 'con_0008' 'con_0009' 'con_0010'};
 %ConNames   = {'Ext' 'Int2' 'Int3' 'Catch' 'Int' 'Ext>Int' 'Int>Ext' 'Int3>Int2' 'Int2>Int3' 'Mean_ExtInt'};
 % ConList = {'con_0001' 'con_0005' 'con_0006' 'con_0007' 'con_0010'};
-% ConNames   = {'Ext' 'Int' 'Ext>Int' 'Int>Ext' 'Mean_ExtInt'};
+% ConNames= {'Ext'      'Int'      'Ext>Int'  'Int>Ext'  'Mean_ExtInt'};
 ConList = {'con_0007' 'con_0010'};
-ConNames   = {'Int>Ext' 'Mean_ExtInt'};
+ConNames= {'Int>Ext'  'Mean_ExtInt'};
 
 if ~istrue(BaselineOnly)
-    inputs = cell(7, 1);
+    inputs = cell(8, 1);
     JobFile = '/home/sysneu/marjoh/scripts/Personalized-Parkinson-Project-Motor/motor/motor_2ndlevel_OneSampleTtests_prog_job.m';
 else
-    inputs = cell(6, 1);
+    inputs = cell(7, 1);
     JobFile = '/home/sysneu/marjoh/scripts/Personalized-Parkinson-Project-Motor/motor/motor_2ndlevel_OneSampleTtests_ba_job.m';
 end
 
 for c = 1:numel(ConList)
     
     if strcmp(ConNames{c}, 'Int>Ext')
-%         Mask = {'/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/HcOn_ExtInt2Int3Catch/Ext_Over_Int_PDOnly.nii'};
-%         Mask = {'/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/HcOn_ExtInt2Int3Catch/Int_Over_Ext_PDOnly.nii'};
-        Mask = {'/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/HcOn_ExtInt2Int3Catch/Task_related_activity_PDonly_mask.nii'};
+        Mask = {'/project/3024006.02/Analyses/Masks/WholeBrain.nii'};
+    elseif strcmp(ConNames{c}, 'Ext>Int')
+        Mask = {'/project/3024006.02/Analyses/Masks/WholeBrain.nii'};
     elseif strcmp(ConNames{c}, 'Mean_ExtInt')
-        Mask = {'/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/HcOn_ExtInt2Int3Catch/Task_related_activity_PDonly_mask.nii'};
+        Mask = {'/project/3024006.02/Analyses/Masks/WholeBrain.nii'};
     end
     
     % Disease progression regressed against brain activity (BA severity treated as a covariate of non-interest)
     % Find images based on pseudos in clinical vars
-    ConDir = fullfile(ANALYSESDir, 'Group', ConList{c}, 'ses-Visit1');
+    ConDir = fullfile(ANALYSESDir, GroupFolder, ConList{c}, 'ses-Visit1');
     PdIms = struct2table(dir(fullfile(ConDir, 'PD_POM*')));
     rID = contains(PdIms.name, ClinVars_subset.pseudonym);
     PdIms_subset = PdIms(rID,1);
@@ -112,18 +158,24 @@ for c = 1:numel(ConList)
     ClinVars_subset.FD = SubInfo.FD(rID);
     inputs{3,1} = table2array(ClinVars_subset(:,5));
     if ~istrue(BaselineOnly)
-        inputs{1,1} = {fullfile(ANALYSESDir, 'Group', ['OneSampleTtest_ClinCorr-Off-' Subscore '-Prog_POM_masked'], ConNames{c})};
+        inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, ['OneSampleTtest_ClinCorr-Off-Prog-' Subscore], ConNames{c})};
         inputs{4,1} = table2array(ClinVars_subset(:,6));
-%         inputs{5,1} = ClinVars_subset.Age;
-        inputs{5,1} = grp2idx(ClinVars_subset.Gender)-1;
-%         inputs{5,1} = ClinVars_subset.FD;
-        inputs{6,1} = Mask;
+        inputs{5,1} = ClinVars_subset.Age;
+        inputs{6,1} = grp2idx(ClinVars_subset.Gender)-1;
+        inputs{7,1} = ClinVars_subset.FD;
+        inputs{8,1} = Mask;
     else
-        inputs{1,1} = {fullfile(ANALYSESDir, 'Group', ['OneSampleTtest_ClinCorr-Off-' Subscore '-BA_POM_masked'], ConNames{c})};
-%         inputs{4,1} = ClinVars_subset.Age;
-        inputs{4,1} = grp2idx(ClinVars_subset.Gender)-1;
-%         inputs{4,1} = ClinVars_subset.FD;
-        inputs{5,1} = Mask;
+        inputs{1,1} = {fullfile(ANALYSESDir, GroupFolder, ['OneSampleTtest_ClinCorr-Off-BA' Subscore], ConNames{c})};
+        inputs{4,1} = ClinVars_subset.Age;
+        inputs{5,1} = grp2idx(ClinVars_subset.Gender)-1;
+        inputs{6,1} = ClinVars_subset.FD;
+        inputs{7,1} = Mask;
+    end
+    if exclude_outliers
+        inputs{1,1} = insertAfter(inputs{1,1}, Subscore, '_NoOutliers');
+    end
+    if ~isempty(Subtype)
+        inputs{1,1} = insertBefore(inputs{1,1}, 'OneSampleTtest', [Subtype '_']);
     end
     %Start with new directory
     if ~exist(char(inputs{1,1}), 'dir')
