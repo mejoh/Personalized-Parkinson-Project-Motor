669cbf3611f3b69593f1ba0729db1ead0afafdb7
Martin Johansson <m.johansson@donders.ru.nl>
2021-11-18 14:10:52 +0100

    Update relabel_categorical_vals.R
    Fixed ParticipantType

--- R/functions/relabel_categorical_vals.R
+++ R/functions/relabel_categorical_vals.R
@@ -10,7 +10,8 @@ relabel_categorical_vals <- function(df){
   levels(df$Gender) <- c('Male', 'Female')
   df$ParkinMedUser <- as.factor(df$ParkinMedUser)                 # Parkinson's medication use
   levels(df$ParkinMedUser) <- c('No','Yes')
-  df$ParticipantType[is.na(df$ParticipantType)] <- 'PD_POM'
+  df$ParticipantType[str_detect(df$Timepoint,'POMVisit*')] <- 3          # Fix the two cases where ParticipantType is undefined
+  df$ParticipantType[str_detect(df$Timepoint,'PITVisit2')] <- 2
   df$ParticipantType <- as.factor(df$ParticipantType)
   levels(df$ParticipantType) <- c('PD_PIT','HC_PIT', 'PD_POM')
   
