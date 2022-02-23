6a049f1b1d012cb6d785cf19ca99342bcc4b64e3
Martin Johansson <m.johansson@donders.ru.nl>
2022-02-23 09:54:19 +0100

    Create compare_count.R
    Compares counts between classifications with a chi-square tests

---
+++ R/functions/compare_count.R
@@ -0,0 +1,26 @@
+# 'dat' is a 2-column data frame in long format
+# Column 1 reflects rater, or classification strategy
+# Column 2 reflects decision, or subtype
+
+compare_count <- function(dat, mosaic=TRUE, simulate.p = FALSE){
+        
+    library(vcd)    
+    library(chisq.posthoc.test)
+        
+    cat('\n<<<Comparison of counts (Chi^2)>>>\n')
+    
+    tab <- table(dat)
+    
+    chi2 <- chisq.test(tab, simulate.p.value = simulate.p, B = 5000)
+    cat('\n<<<Observed>>>\n')
+    print(chi2$observed)
+    cat('\n<<<Expected>>> \n')
+    print(chi2$expected)
+    cat('\n<<<Statistics>>> \n')
+    print(chi2)
+    cat('\n<<<Post hoc>>> \n')
+    print(chisq.posthoc.test(table(dat)))
+    
+    if(mosaic) mosaic(tab)
+    
+}
