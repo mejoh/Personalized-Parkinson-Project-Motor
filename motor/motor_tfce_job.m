c36eb5843f11bc9ec5b3284b806a5b03dc61a6d4
Martin Johansson <m.johansson@donders.ru.nl>
2022-02-23 09:51:03 +0100

    Create motor_tfce_job.m

---
+++ motor/motor_tfce_job.m
@@ -0,0 +1,14 @@
+%-----------------------------------------------------------------------
+% Job saved on 10-Feb-2022 11:02:02 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.tfce_estimate.spmmat = '<UNDEFINED>';
+matlabbatch{1}.spm.tools.tfce_estimate.mask = '';
+matlabbatch{1}.spm.tools.tfce_estimate.conspec.titlestr = '';
+matlabbatch{1}.spm.tools.tfce_estimate.conspec.contrasts = '<UNDEFINED>';
+matlabbatch{1}.spm.tools.tfce_estimate.conspec.n_perm = '<UNDEFINED>';
+matlabbatch{1}.spm.tools.tfce_estimate.nuisance_method = 2;
+matlabbatch{1}.spm.tools.tfce_estimate.tbss = 0;
+matlabbatch{1}.spm.tools.tfce_estimate.E_weight = 0.5;
+matlabbatch{1}.spm.tools.tfce_estimate.singlethreaded = 0;
