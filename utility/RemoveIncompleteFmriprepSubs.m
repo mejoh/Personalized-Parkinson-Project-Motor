9fed00da55672cec1e1e73cdbe5f17a0e6d70467
Martin Johansson <m.johansson@donders.ru.nl>
2022-05-10 16:25:24 +0200

    Update RemoveIncompleteFmriprepSubs.m

--- utility/RemoveIncompleteFmriprepSubs.m
+++ utility/RemoveIncompleteFmriprepSubs.m
@@ -18,29 +18,29 @@ for n=1:length(sub)
     compare_bids_and_fmriprep(bidsdir, fmriprepdir, sub{n})
 end
 
-function compare_bids_and_fmriprep(bidsdir, fmriprepdir, sub)
+function compare_bids_and_fmriprep(bidsdir, fmriprepdir, subject)
 
 % Find relevant files
-bidscontents=cellstr(spm_select('FPList', fullfile(bidsdir, sub), 'dir', '^ses.*'));
-fmriprepcontents=cellstr(spm_select('FPList', fullfile(fmriprepdir, sub), 'dir', '^ses.*'));
-fmriprephtml=cellstr(spm_select('FPList', fmriprepdir, ['^' sub '.*']));
+bidscontents=cellstr(spm_select('FPList', fullfile(bidsdir, subject), 'dir', '^ses.*'));
+fmriprepcontents=cellstr(spm_select('FPList', fullfile(fmriprepdir, subject), 'dir', '^ses.*'));
+fmriprephtml=cellstr(spm_select('FPList', fmriprepdir, ['^' subject '.*']));
 
 % Report number of files
 lb=length(bidscontents);
 lf=length(fmriprepcontents);
 lfh=length(fmriprephtml);
-%fprintf('%s, sessions in bids: %i, sessions in fmriprep: %i, html file: %i \n', sub, lb, lf, lfh)
+fprintf('%s, sessions in bids: %i, sessions in fmriprep: %i, html file: %i \n', subject, lb, lf, lfh)
 
 % Check whether there are more sessions in bids than in fmriprep
 % If there is, then the subject needs to be re-preprocessed
 % Remove fmriprep directory and html file
 if lb > lf
     
-    fprintf('    %s \n', sub)
+    fprintf('    %s \n', subject)
     
     fprintf('    Number of bidsfolders (%i) does not match number of fmriprep sessions (%i) \n', lb, lf)
     fprintf('    Removing fmriprep sessions \n')
-    rmdir(fullfile(fmriprepdir, sub), 's')
+    rmdir(fullfile(fmriprepdir, subject), 's')
     
     fprintf('    Removing fmriprep html file \n')
     delete(char(fmriprephtml))
