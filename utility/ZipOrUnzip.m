afa4f6509d4414743d200384cd9a6cbedd54c4c1
mejoh <m.johansson@donders.ru.nl>
2022-11-17 13:41:32 +0100

    Update ZipOrUnzip.m

--- utility/ZipOrUnzip.m
+++ utility/ZipOrUnzip.m
@@ -1,28 +1,26 @@
 function ZipOrUnzip()
 
-% BIDSDir = '/project/3022026.01/pep/bids';
-% FMRIPREP = fullfile(BIDSDir, 'derivatives', 'fmriprep');
 dAna = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem';
 Sub = cellstr(spm_select('List', fullfile(dAna), 'dir', '^sub-POM.*'));
+pat = '^ssub.*task-motor_acq-MB6.*preproc_bold.nii.*';
+% pat = '^beta_0.*.nii.*';
 fprintf('Number of subjects found: %i\n', numel(Sub))
 
 for n = 1:numel(Sub)
-%     Visit = cellstr(spm_select('List', fullfile(FMRIPREP, Sub{n}), 'dir', 'ses-Visit*'));
     Visit = cellstr(spm_select('List', fullfile(dAna, Sub{n}), 'dir', 'ses-.*Visit.*'));
     for v = 1:numel(Visit)
-%         dFunc = fullfile(FMRIPREP, Sub{n}, Visit{v}, 'func');
         dFunc = fullfile(dAna, Sub{n}, Visit{v});
-        img = spm_select('FPList', dFunc, '^ssub.*task-motor_acq-MB6.*preproc_bold.nii.*');
-%         if exist(img, 'file') && contains(img, 'nii.gz')
-        if exist(img, 'file') && endsWith(img, 'nii')
-%             disp(['Unzipping: ' img])
-%             gunzip(img, fileparts(img))
-            disp(['Zipping: ' img])
-            gzip(img, fileparts(img))
-            delete(img)
-        else
-%             fprintf('Skipping %s: No image or already unzipped \n', Sub{n})
-            fprintf('Skipping %s: No image or already zipped \n', Sub{n})
+%         dFunc = fullfile(dAna, Sub{n}, Visit{v}, '1st_level');
+        img = cellstr(spm_select('FPList', dFunc, pat));
+%         img = img(16:numel(img));       % Remove 1-15 from list since these might be useful
+        for i = 1:numel(img)
+            if exist(img{i}, 'file') && endsWith(img{i}, 'nii')
+                disp(['Zipping: ' img{i}])
+                gzip(img{i}, fileparts(img{i}))
+                delete(img{i})
+%             else
+%                 fprintf('Skipping %s: No image or already zipped \n', Sub{n})
+            end
         end
     end
 end
