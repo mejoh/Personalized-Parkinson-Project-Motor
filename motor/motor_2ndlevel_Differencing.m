de06e24137b9ec7ec53588a52707b8d025d79645
mejoh <m.johansson@donders.ru.nl>
2023-01-10 10:03:20 +0100

    Update motor_2ndlevel_Differencing.m

--- motor/motor_2ndlevel_Differencing.m
+++ motor/motor_2ndlevel_Differencing.m
@@ -5,8 +5,9 @@
 % swapping is consistent between images, then takes the T2 - T1 difference.
 
 dGroup = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group';
-con = {'con_0001','con_0002','con_0003','con_0004',...
-    'con_0010', 'con_0012', 'con_0013', 'con_0008'};
+con = {'con_0001','con_0002','con_0003','con_0004', 'con_0005',...
+    'con_0010', 'con_0007', 'con_0012', 'con_0013'};
+con = {'con_0005'};
 
 for c = 1:numel(con)
     
@@ -14,8 +15,8 @@ for c = 1:numel(con)
 
     % Ensure that con has both baseline and follow-up directories
     dCon = fullfile(dGroup,con{c});
-    s1 = spm_select('FPList',dCon,'dir','ses-Visit1');
-    s2 = spm_select('FPList',dCon,'dir','ses-Visit2');
+    s1 = spm_select('FPList',dCon,'dir','^ses-Visit1');
+    s2 = spm_select('FPList',dCon,'dir','^ses-Visit2');
     try
         s1_s2 = [exist(s1,'dir'); exist(s2,'dir')];
     catch
@@ -25,6 +26,10 @@ for c = 1:numel(con)
     % Select available images
     s1_imgs = cellstr(spm_select('List',s1,'.*nii'));
     s2_imgs = cellstr(spm_select('List',s2,'.*nii'));
+    
+    s1_imgs = s1_imgs(~contains(s1_imgs,'PD_PIT'));
+    s2_imgs = s2_imgs(~contains(s2_imgs,'PD_PIT'));
+    
     fprintf('Found %i baseline images \n', numel(s1_imgs))
     fprintf('Found %i follow-up images \n', numel(s2_imgs))
     
@@ -34,6 +39,18 @@ for c = 1:numel(con)
  		mkdir(dOut)
  	else
  		delete(fullfile(dOut,'*.*'))
+    end
+    dOutS1 = fullfile(dCon,'COMPLETE_ses-Visit1');
+    if ~exist(dOutS1,'dir')
+ 		mkdir(dOutS1)
+ 	else
+ 		delete(fullfile(dOutS1,'*.*'))
+    end
+    dOutS2 = fullfile(dCon,'COMPLETE_ses-Visit2');
+    if ~exist(dOutS2,'dir')
+ 		mkdir(dOutS2)
+ 	else
+ 		delete(fullfile(dOutS2,'*.*'))
     end
     for i = 1:numel(s2_imgs)
         
@@ -70,8 +87,16 @@ for c = 1:numel(con)
         imvol = spm_vol([fullfile(s2,b);fullfile(s1,a)]);
         
         % Take difference between follow-up and baseline
-        output_img = fullfile(dOut, strrep(b,'Visit2','VisitDiff'));
+        if(contains(b,'Visit2'))
+            output_img = fullfile(dOut, strrep(b,'Visit2','VisitDiff'));
+        elseif (contains(b,'Visit3'))
+            output_img = fullfile(dOut, strrep(b,'Visit3','VisitDiff'));
+        end
         Q = spm_imcalc(imvol, output_img, 'i2-i1');
+        
+        % Save complete case images
+        copyfile(imvol(1).fname, dOutS2)
+        copyfile(imvol(2).fname, dOutS1)
         
     end
 
