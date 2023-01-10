e02192be642d4de9c438f2183a4cc790cde3b540
mejoh <m.johansson@donders.ru.nl>
2023-01-10 10:04:12 +0100

    Update motor_copycontrasts.m

--- motor/motor_copycontrasts.m
+++ motor/motor_copycontrasts.m
@@ -10,15 +10,20 @@ end
 %% Directories
 
 consession = [session(1:4) session(8:13)];
-FDThresh = 1;
-ConList = {'con_0012', 'con_0013', 'con_0014', 'con_0015',...
-    'con_0001' 'con_0002' 'con_0003' 'con_0004' 'con_0005' 'con_0006' 'con_0007'  'con_0008'  'con_0009'  'con_0010'};
+FDThresh = 10;
+ConList = {
+    'con_0001' 'con_0002' 'con_0003' 'con_0004' 'con_0005'...
+    'con_0010' 'con_0007' 'con_0012', 'con_0013'};
+ConList = {'con_0005'};
 ANALYSESDir = '/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem';
 BIDSDir = '/project/3022026.01/pep/bids';
-ClinicalConfs = readtable('/project/3024006.02/Data/matlab/ClinVars_select_mri5.csv');
+ClinicalConfs = readtable('/project/3024006.02/Data/matlab/Longitudinal_ClinVars_2022-12-06.csv');
 Sub = cellstr(spm_select('List', fullfile(ANALYSESDir), 'dir', '^sub-POM.*'));
 fprintf('Number of subjects processed: %i\n', numel(Sub))
 
+%% Selection
+
+% 1st-level output
 Sel = true(numel(Sub),1);
 for n = 1:numel(Sub)
     dir = spm_select('FPList', fullfile(ANALYSESDir, Sub{n}), 'dir', session);
@@ -26,27 +31,7 @@ for n = 1:numel(Sub)
         Sel(n) = false;
     end
 end
-Sub = Sub(Sel);
-
-
-%% Selection
-
-% Take the last run for each participant's session(s)
-% (necessary for finding the correct json and conf files)
-Run = {};
-Session = {};
-Sub2 = {};
-for n = 1:numel(Sub)
-    Visit = cellstr(spm_select('List', fullfile(ANALYSESDir, Sub{n}), 'dir', session));
-    for v = 1:numel(Visit)
-        Sub2 = [Sub2; {Sub{n}}];
-        Session = [Session; {Visit{v}}];
-        Run = [Run; {num2str(FindLastRun(BIDSDir, Sub{n}, Visit{v}, 'motor', 'MB6'))}];
-    end
-end
-SubInfo.Sub = Sub2;
-SubInfo.Session = Session;
-SubInfo.Run = Run;
+SubInfo.Sub = Sub(Sel);
 
 %% Collect events.json and confound files
 
@@ -54,11 +39,12 @@ SubInfo.JsonFiles = cell(size(SubInfo.Sub));
 SubInfo.ConfFiles = cell(size(SubInfo.Sub));
 
 for n = 1:numel(SubInfo.Sub)
-    SubInfo.JsonFiles{n} = spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, SubInfo.Session{n}, 'beh'), ['.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_events\.json$']);
-    SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, SubInfo.Session{n}), ['^.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_desc-confounds_timeseries3.mat$']);
-    if isempty(SubInfo.ConfFiles{n})
-        SubInfo.ConfFiles{n} = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, SubInfo.Session{n}), ['^.*task-motor_acq-MB6_run-' SubInfo.Run{n} '_desc-confounds_timeseries2.mat$']);
-    end
+    confs = spm_select('FPList', fullfile(ANALYSESDir, SubInfo.Sub{n}, session), '^.*task-motor_acq-MB6_run-.*_desc-confounds_timeseries.*.mat$');
+    dims = size(confs);
+    SubInfo.ConfFiles{n} = confs(dims(1),:);
+    json = spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, session, 'beh'), '.*task-motor_acq-MB6_run-.*_events\.json$');
+    dims = size(json);
+    SubInfo.JsonFiles{n} = json(dims(1),:);
 end
 
 %% Group and handedness
@@ -70,9 +56,9 @@ SubInfo.PercentageCorrect = strings(size(SubInfo.Sub));
 for n = 1:numel(SubInfo.Sub)
     Json = fileread(SubInfo.JsonFiles{n});
     DecodedJson = jsondecode(Json);
-    if exist(spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, SubInfo.Session{n}), 'dir', 'dwi'), 'dir') && contains(SubInfo.Session{n}, 'PIT')
+    if exist(spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, session), 'dir', 'dwi'), 'dir') && contains(session, 'PIT')
         SubInfo.Group(n) = 'HC_PIT';
-    elseif ~exist(spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, SubInfo.Session{n}), 'dir', 'dwi'), 'dir') && contains(SubInfo.Session{n}, 'PIT')
+    elseif ~exist(spm_select('FPList', fullfile(BIDSDir, SubInfo.Sub{n}, session), 'dir', 'dwi'), 'dir') && contains(session, 'PIT')
         SubInfo.Group(n) = 'PD_PIT';
     else
         SubInfo.Group(n) = 'PD_POM';
@@ -103,15 +89,7 @@ for n = 1:numel(SubInfo.Sub)
     end
 end
 fprintf('%i participants excluded due to excessive motion \n', sum(Sel == 0))
-SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Session = SubInfo.Session(Sel);
-SubInfo.Run = SubInfo.Run(Sel);
-SubInfo.JsonFiles = SubInfo.JsonFiles(Sel);
-SubInfo.ConfFiles = SubInfo.ConfFiles(Sel);
-SubInfo.Group = SubInfo.Group(Sel);
-SubInfo.RespondingHand = SubInfo.RespondingHand(Sel);
-SubInfo.FD = SubInfo.FD(Sel);
-SubInfo.PercentageCorrect = SubInfo.PercentageCorrect(Sel);
+SubInfo = subset_subinfo(SubInfo, Sel);
 
 % Percentage correct responses in external
 Sel = true(size(SubInfo.Sub));
@@ -122,15 +100,37 @@ for n = 1:numel(SubInfo.Sub)
     end
 end
 fprintf('%i participants excluded due to poor performance \n', sum(Sel == 0))
-SubInfo.Sub = SubInfo.Sub(Sel);
-SubInfo.Session = SubInfo.Session(Sel);
-SubInfo.Run = SubInfo.Run(Sel);
-SubInfo.JsonFiles = SubInfo.JsonFiles(Sel);
-SubInfo.ConfFiles = SubInfo.ConfFiles(Sel);
-SubInfo.Group = SubInfo.Group(Sel);
-SubInfo.RespondingHand = SubInfo.RespondingHand(Sel);
-SubInfo.FD = SubInfo.FD(Sel);
-SubInfo.PercentageCorrect = SubInfo.PercentageCorrect(Sel);
+SubInfo = subset_subinfo(SubInfo, Sel);
+
+% Quality control: outlier exclusion
+Outliers = readtable('/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Exclusions.csv');
+% Lenient option:
+baseid = contains(Outliers.visit, session) & Outliers.definitive_exclusions == 1;
+% Conservative option: 
+% baseid = contains(Outliers.visit, session);
+Outliers = Outliers(baseid,:);
+
+Sel = true(size(SubInfo.Sub));
+for n = 1:numel(SubInfo.Sub)
+    if contains(SubInfo.Sub{n}, Outliers.pseudonym)
+       Sel(n) = false;
+    fprintf('Excluding outlier: %s %s \n', SubInfo.Sub{n}, SubInfo.Group{n})
+    end
+end
+fprintf('%i outliers have been excluded \n', length(Sel) - sum(Sel))
+SubInfo = subset_subinfo(SubInfo, Sel);
+
+% Diagnosis
+Sel = true(size(SubInfo.Sub));
+for n = 1:numel(SubInfo.Sub)
+    subid = find(contains(ClinicalConfs.pseudonym, SubInfo.Sub{n}));
+    if ClinicalConfs.Misdiagnosis(subid)>0
+        fprintf('Misdiagnosis as non-PD, excluding %s...\n', SubInfo.Sub{n})
+        Sel(n) = false;
+    end
+end
+fprintf('%i subjects have a non-PD diagnosis, excluding these now...\n', length(Sel) - sum(Sel))
+SubInfo = subset_subinfo(SubInfo, Sel);
 
 fprintf('%i subjects will be copied\n', length(SubInfo.Sub))
 
@@ -145,7 +145,11 @@ for c = 1:numel(ConList)
 %         delete(fullfile(ConDir, '*.*'));
     end
     if ~exist(fullfile(ConDir, consession), 'dir')
-        mkdir(fullfile(ConDir, consession))
+        if contains(consession, 'Visit1')
+            mkdir(fullfile(ConDir, 'ses-Visit1'))
+        else
+            mkdir(fullfile(ConDir, 'ses-Visit2'))
+        end
 %     else
 %         delete(fullfile(ConDir, consession, '*.*'));
     end
