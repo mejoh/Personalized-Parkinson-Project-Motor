582eca6badb0a59effc06eda86183689fcb7f3f8
mejoh <m.johansson@donders.ru.nl>
2023-01-25 16:15:58 +0100

    Update merge_csv_to_file.R
    Altered to accomodate inflammation markers

--- R/functions/merge_csv_to_file.R
+++ R/functions/merge_csv_to_file.R
@@ -1,15 +1,20 @@
 merge_csv_to_file <- function(fps, outputname){
   
-  df <- read_csv(fps[1]) %>%
-    mutate(across(everything(), as.character))
-  names(df) <- gsub("\\_[1-2]", "", names(df))    # PIT subjects have _[1-2] at the end of colnames. Can safely be removed
-  for(n in fps){
-    tmp <- read_csv(n) %>%
-      mutate(across(everything(), as.character))
-    names(tmp) <- gsub("\\_[1-2]", "", names(tmp))
-    df <- full_join(df,tmp)
-  }
-
-  write_csv(df, outputname)
+        df <- read_csv(fps[1], show_col_types = FALSE) %>%
+                mutate(across(everything(), as.character))
+        for(n in fps){
+                msg <- paste('Writing', n, '\n')
+                cat(msg)
+                tmp <- read_csv(n, show_col_types = FALSE) %>%
+                        mutate(across(everything(), as.character))
+                if(str_detect(n, 'ses-PITVisit')){
+                        names(tmp) <- gsub("\\_[1-2]", "", names(tmp)) # PIT subjects have _[1-2] at the end of colnames. Can safely be removed
+                }
+                df <- full_join(df, tmp, show_col_types = FALSE)
+        }
+        
+        msg <- paste('Writing', outputname, '\n')
+        cat(msg)
+        write_csv(df, outputname)
   
 }
