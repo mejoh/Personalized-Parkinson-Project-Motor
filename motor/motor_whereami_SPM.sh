b856bd787946c8e41780b558f71da7931967e764
mejoh <m.johansson@donders.ru.nl>
2023-03-09 15:37:55 +0100

    Create motor_whereami_SPM.sh

---
+++ motor/motor_whereami_SPM.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/HcOn_x_ExtInt2Int3Catch_NoOutliers
+# dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/HcSubtypes_x_ExtInt2Int3Catch_NoOutliers
+#dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/Subtypes_x_ExtInt2Int3Catch_NoOutliers
+# dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/OneSampleTtest_ClinCorr-Off-BABradySum_NoOutliers/Int2gtExt
+# dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/OneSampleTtest_ClinCorr-Off-BABradySum_NoOutliers/Int3gtExt
+dir=/project/3024006.02/Analyses/DurAvg_ReAROMA_PMOD_TimeDer_Trem/Group/Baseline/OneSampleTtest_ClinCorr-Off-BABradySum_NoOutliers/Mean_ExtInt
+
+if [  ! "$dir" ]; then
+
+	echo -e "\nProvide a directory please! \n"
+	exit 1
+	
+fi
+
+module unload afni; module load afni/2022
+
+cd $dir
+mkdir -p whereami
+rm whereami/*
+masks=`ls x_*_Mask.nii`
+for i in ${masks[@]}; do
+
+	filename="${i%.*}"
+	echo -e "\n${filename} \n"
+	
+	3dcopy $i whereami/$filename
+	filename2=${filename}+tlrc.
+	3drefit -space MNI whereami/${filename2}
+	whereami -space MNI -spm -atlas CA_MPM_22_MNI -omask whereami/${filename2} > whereami/${filename}_a-CA_MPM_22-MNI.txt
+	whereami -space MNI -spm -atlas CA_ML_18_MNI -omask whereami/${filename2} > whereami/${filename}_a-CA_ML_18_MNI.txt
+	whereami -space MNI -spm -atlas MNI_Glasser_HCP_v1.0 -omask whereami/${filename2} > whereami/${filename}_a-MNI_Glasser_HCP_v1.0.txt
+	whereami -space MNI -spm -atlas MNI_VmPFC -omask whereami/${filename2} > whereami/${filename}_a-MNI_VmPFC.txt
+	rm whereami/${filename2}*
+	
+done
