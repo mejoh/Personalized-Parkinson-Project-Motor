0afcc18db7e7ad8edc6c547e6a2aaad73fe72e24
mejoh <m.johansson@donders.ru.nl>
2023-06-23 08:24:37 +0200

    Update to cloudplots

--- R/functions/raincloudplot_1Factor.R
+++ R/functions/raincloudplot_1Factor.R
@@ -1,6 +1,6 @@
 raincloudplot_1Factor <- function(data, y, groupvar, title='', xlab='', ylab='',
                                   xticklabs='', print_summary=FALSE, provided_summary='None',
-                                  color_scheme='mako'){
+                                  color_scheme='mako', cloud=TRUE){
     
         
         library(extrafont)
@@ -21,68 +21,99 @@ raincloudplot_1Factor <- function(data, y, groupvar, title='', xlab='', ylab='',
                 print(sumrepdat)
         }
         
-        # Generate raincloud
-        # ggplot(data, aes_string(x = groupvar, y = y)) +
-        #     geom_flat_violin(aes_string(fill = groupvar),
-        #                      position = position_nudge(x = .1, y = 0), 
-        #                      adjust = 1.5, 
-        #                      trim = FALSE, 
-        #                      alpha = .5, 
-        #                      colour = 'black', show.legend = FALSE) +
-        #     geom_jitter(aes_string(x = paste('as.numeric(',groupvar,')-.1', sep=''), y = y, colour = groupvar),
-        #                 size = 2,
-        #                 shape = 19,
-        #                 width = .01,
-        #                 alpha = .5, color='black', show.legend = FALSE) +
-        #     geom_boxplot(aes_string(x = groupvar, y = y, fill = groupvar),
-        #                  outlier.shape = NA, 
-        #                  alpha = .7, 
-        #                  width = .1, 
-        #                  colour = "black", show.legend = FALSE) +
-        #     geom_line(data = sumrepdat,
-        #               aes_string(x = paste('as.numeric(',groupvar,')+.1'), y = 'mean'),
-        #               linetype = 3, size = .9, show.legend = FALSE, color='black') +
-        #     geom_point(data = sumrepdat, 
-        #                aes_string(x = paste('as.numeric(',groupvar,')+.1', sep=''), y = 'mean',
-        #                           group = groupvar), shape = 18, show.legend = FALSE, size=3, color='black') +
-        #     geom_errorbar(data = sumrepdat, 
-        #                   aes_string(x = paste('as.numeric(',groupvar,')+.1',sep=''), y = 'mean',
-        #                              ymin = 'mean-se', ymax = 'mean+se', group = groupvar),
-        #                   width = .07, show.legend = FALSE, color='black') +
-        #     scale_x_discrete(labels = xticklabs) +
-        #     scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
-        #     scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
-        #     ggtitle(title) + xlab(xlab) + ylab(ylab) + 
-        #     coord_cartesian(xlim = c(1.2, NA), clip = "off") +
-        #     theme_cowplot(font_size = 20)
-        ggplot(data, aes_string(x = groupvar, y = y)) +
-                geom_flat_violin(aes_string(fill = groupvar),
-                                 position = position_nudge(x = .25, y = 0), 
-                                 adjust = 1.5, 
-                                 trim = TRUE, 
-                                 alpha = .5, 
-                                 colour = 'black', show.legend = FALSE, width = 0.4, linewidth=1.2) +
-                geom_point(aes_string(x = paste('as.numeric(',groupvar,')-0', sep=''), y = y, fill = groupvar),
-                           size = 2,
-                           shape = 19,
-                           alpha = .3, show.legend = FALSE,
-                           position = position_jitterdodge(dodge.width = .6, jitter.width = .2),color='black') +
-                geom_boxplot(aes_string(x = groupvar, y = y, fill = groupvar),
-                             outlier.shape = NA, 
-                             alpha = .7, 
-                             width = .4, 
-                             size=1.2,
-                             colour = "black", show.legend = FALSE) +
-                geom_point(data = sumrepdat, 
-                           aes_string(x = paste('as.numeric(',groupvar,')+0', sep=''), y = 'mean',
-                                      group = groupvar, color = groupvar),
-                           shape = 18, size=5, show.legend = FALSE,
-                           position = position_dodge(width = .6), color='yellow3') +
-                scale_x_discrete(labels = xticklabs) +
-                scale_colour_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1) +
-                scale_fill_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1, alpha = .8) +
-                ggtitle(title) + xlab(xlab) + ylab(ylab) + 
-                coord_cartesian(xlim = c(1.2, NA), clip = "off") +
-                theme_cowplot(font_size = 16, font_family = 'Calibri')
+        if(cloud){
+                
+                # Raincloudplot
+                ggplot(data, aes(x = !!ensym(groupvar), y = !!ensym(y))) +
+                        geom_flat_violin(aes(fill = !!ensym(groupvar)),
+                                         position = position_nudge(x = .25, y = 0), 
+                                         adjust = 1.5, 
+                                         trim = TRUE, 
+                                         alpha = .5, 
+                                         colour = 'black', show.legend = FALSE, width = 0.4, linewidth=1.2) +
+                        geom_point(aes(x = !!ensym(groupvar), y = !!ensym(y), fill = !!ensym(groupvar)),
+                                   size = 2,
+                                   shape = 19,
+                                   alpha = .3, show.legend = FALSE,
+                                   position = position_jitterdodge(dodge.width = .6, jitter.width = .2),color='black') +
+                        geom_boxplot(aes(x = !!ensym(groupvar), y = !!ensym(y), fill = !!ensym(groupvar)),
+                                     outlier.shape = NA, 
+                                     alpha = .7, 
+                                     width = .4, 
+                                     size=1.2,
+                                     colour = "black", show.legend = FALSE) +
+                        geom_point(data = sumrepdat, 
+                                   aes(x = !!ensym(groupvar), y = mean,
+                                       group = !!ensym(groupvar)),
+                                   shape = 18, size=3, show.legend = FALSE,
+                                   position = position_dodge(width = .6), color='grey') +
+                        scale_x_discrete(labels = xticklabs) +
+                        scale_colour_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1) +
+                        scale_fill_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1, alpha = .8) +
+                        ggtitle(title) + xlab(xlab) + ylab(ylab) + 
+                        coord_cartesian(xlim = c(1.2, NA), clip = "off") +
+                        theme_cowplot(font_size = 16, font_family = 'Calibri')
+        }else{
+                # Box-scatter plot
+                ggplot(data, aes(x = !!ensym(groupvar), y = !!ensym(y))) +
+                        geom_point(aes(x = !!ensym(groupvar), y = !!ensym(y), fill = !!ensym(groupvar)),
+                                   size = 2,
+                                   shape = 19,
+                                   alpha = .3, show.legend = FALSE,
+                                   position = position_jitterdodge(dodge.width = .6, jitter.width = .2),color='black') +
+                        geom_boxplot(aes(x = !!ensym(groupvar), y = !!ensym(y), fill = !!ensym(groupvar)),
+                                     outlier.shape = NA, 
+                                     alpha = .7, 
+                                     width = .4, 
+                                     size=1.2,
+                                     colour = "black", show.legend = FALSE) +
+                        geom_point(data = sumrepdat, 
+                                   aes(x = !!ensym(groupvar), y = mean,
+                                       group = !!ensym(groupvar)),
+                                   shape = 18, size=3, show.legend = FALSE,
+                                   position = position_dodge(width = .6), color='grey') +
+                        scale_x_discrete(labels = xticklabs) +
+                        scale_colour_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1) +
+                        scale_fill_viridis_d(option=color_scheme, begin = 0, end = .6, direction = 1, alpha = .8) +
+                        ggtitle(title) + xlab(xlab) + ylab(ylab) + 
+                        theme_cowplot(font_size = 16, font_family = 'Calibri')
+        }
     
 }
+
+
+
+# Generate raincloud
+# ggplot(data, aes_string(x = groupvar, y = y)) +
+#     geom_flat_violin(aes_string(fill = groupvar),
+#                      position = position_nudge(x = .1, y = 0), 
+#                      adjust = 1.5, 
+#                      trim = FALSE, 
+#                      alpha = .5, 
+#                      colour = 'black', show.legend = FALSE) +
+#     geom_jitter(aes_string(x = paste('as.numeric(',groupvar,')-.1', sep=''), y = y, colour = groupvar),
+#                 size = 2,
+#                 shape = 19,
+#                 width = .01,
+#                 alpha = .5, color='black', show.legend = FALSE) +
+#     geom_boxplot(aes_string(x = groupvar, y = y, fill = groupvar),
+#                  outlier.shape = NA, 
+#                  alpha = .7, 
+#                  width = .1, 
+#                  colour = "black", show.legend = FALSE) +
+#     geom_line(data = sumrepdat,
+#               aes_string(x = paste('as.numeric(',groupvar,')+.1'), y = 'mean'),
+#               linetype = 3, size = .9, show.legend = FALSE, color='black') +
+#     geom_point(data = sumrepdat, 
+#                aes_string(x = paste('as.numeric(',groupvar,')+.1', sep=''), y = 'mean',
+#                           group = groupvar), shape = 18, show.legend = FALSE, size=3, color='black') +
+#     geom_errorbar(data = sumrepdat, 
+#                   aes_string(x = paste('as.numeric(',groupvar,')+.1',sep=''), y = 'mean',
+#                              ymin = 'mean-se', ymax = 'mean+se', group = groupvar),
+#                   width = .07, show.legend = FALSE, color='black') +
+#     scale_x_discrete(labels = xticklabs) +
+#     scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
+#     scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
+#     ggtitle(title) + xlab(xlab) + ylab(ylab) + 
+#     coord_cartesian(xlim = c(1.2, NA), clip = "off") +
+#     theme_cowplot(font_size = 20)
--- R/functions/raincloudplot_2Factor.R
+++ R/functions/raincloudplot_2Factor.R
@@ -1,7 +1,7 @@
 raincloudplot_2Factor <- function(data, y, groupvar, title='', xlab='', ylab='',
                                   xticklabs='', legend_title='', legend_labels='',
                                   print_summary = FALSE, provided_summary = 'None',
-                                  color_scheme='mako', legend_position = ''){
+                                  color_scheme='mako', legend_position = '', cloud = TRUE){
     
         
         library(extrafont)
@@ -25,65 +25,30 @@ raincloudplot_2Factor <- function(data, y, groupvar, title='', xlab='', ylab='',
                 print(sumrepdat)
         }
         
-        # Generate raincloud
-        # ggplot(data, aes_string(x = g2, y = y, fill = g1)) +
-        #         geom_flat_violin(aes_string(fill = g1),
-        #                          position = position_nudge(x = .1, y = 0), 
-        #                          adjust = 1.5, 
-        #                          trim = FALSE, 
-        #                          alpha = .5, 
-        #                          colour = 'black') +
-        #         geom_jitter(aes_string(x = paste('as.numeric(',g2,')-.1', sep=''), y = y, colour = g1),
-        #                     size = 2,
-        #                     shape = 19,
-        #                     width = .04,
-        #                     alpha = .8, show.legend = FALSE) +
-        #         geom_boxplot(aes_string(x = g2, y = y, fill = g1),
-        #                      outlier.shape = NA, 
-        #                      alpha = .7, 
-        #                      width = .1, 
-        #                      colour = "black", show.legend = FALSE) +
-        #         geom_line(data = sumrepdat,
-        #                   aes_string(x = paste('as.numeric(',g2,')+.15'), y = 'mean', group = g1, color = g1),
-        #                   linetype = 3, size = .9, show.legend = FALSE) +
-        #         geom_point(data = sumrepdat, 
-        #                    aes_string(x = paste('as.numeric(',g2,')+.1', sep=''), y = 'mean',
-        #                               group = g1, color = g1), shape = 18, size=3, show.legend = FALSE) +
-        #         geom_errorbar(data = sumrepdat, 
-        #                       aes_string(x = paste('as.numeric(',g2,')+.1',sep=''), y = 'mean',
-        #                                  ymin = 'mean-se', ymax = 'mean+se', group = g1, color = g1),
-        #                       width = .07, show.legend = FALSE) +
-        #         scale_x_discrete(labels = xticklabs) +
-        #         scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
-        #         scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, alpha = .8,
-        #                              direction = -1, labels = legend_labels) +
-        #         ggtitle(title) + xlab(xlab) + ylab(ylab) + 
-        #         guides(fill=guide_legend(title=legend_title, reverse = FALSE)) + 
-        #         coord_cartesian(xlim = c(1.2, NA), clip = "off") +
-        #         theme_cowplot(font_size = 20)
-        ggplot(data, aes_string(x = g2, y = y, fill = g1)) +
-                geom_flat_violin(aes_string(fill = g1),
+        if(cloud){
+            ggplot(data, aes(x = !!ensym(g2), y = !!ensym(y), fill = !!ensym(g1))) +
+                geom_flat_violin(aes(fill = !!ensym(g1)),
                                  position = position_nudge(x = .34, y = 0), 
                                  adjust = 1.5, 
                                  trim = TRUE, 
                                  alpha = .5, 
-                                 colour = 'black', width = 0.3,size=0.8) +
-                geom_point(aes_string(x = paste('as.numeric(',g2,')-0', sep=''), y = y, colour = g1),
-                            size = 1.5,
-                            shape = 19,
-                            alpha = .3, show.legend = FALSE,
+                                 colour = 'black', width = 0.3,linewidth=0.6) +
+                geom_point(aes(x = !!ensym(g2), y = !!ensym(y), colour = !!ensym(g1)),
+                           size = 1.5,
+                           shape = 19,
+                           alpha = .3, show.legend = FALSE,
                            position = position_jitterdodge(dodge.width = .6, jitter.width = .1)) +
-                geom_boxplot(aes_string(x = g2, y = y, fill = g1),
+                geom_boxplot(aes(x = !!ensym(g2), y = !!ensym(y), fill = !!ensym(g1)),
                              outlier.shape = NA, 
                              alpha = .7, 
                              width = .6, 
-                             size=0.8,
+                             size=0.6,
                              colour = "black", show.legend = FALSE) +
                 geom_point(data = sumrepdat, 
-                           aes_string(x = paste('as.numeric(',g2,')+0', sep=''), y = 'mean',
-                                      group = g1, color = g1),
-                           shape = 18, size=4, show.legend = FALSE,
-                           position = position_dodge(width = .6), color='yellow3') +
+                           aes(x = !!ensym(g2), y = mean,
+                               group = !!ensym(g1), color = !!ensym(g1)),
+                           shape = 18, size=3, show.legend = FALSE,
+                           position = position_dodge(width = .6), color='grey') +
                 scale_x_discrete(labels = xticklabs) +
                 scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
                 scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, alpha = .8,
@@ -95,5 +60,73 @@ raincloudplot_2Factor <- function(data, y, groupvar, title='', xlab='', ylab='',
                 theme_cowplot(font_size = 16, font_family = 'Calibri') +
                 theme(legend.position = legend_position,
                       legend.key.size = unit(0.5,'cm'))
+        }else{
+            ggplot(data, aes(x = !!ensym(g2), y = !!ensym(y), fill = !!ensym(g1))) +
+                geom_point(aes(x = !!ensym(g2), y = !!ensym(y), colour = !!ensym(g1)),
+                           size = 1.5,
+                           shape = 19,
+                           alpha = .3, show.legend = FALSE,
+                           position = position_jitterdodge(dodge.width = .6, jitter.width = .1)) +
+                geom_boxplot(aes(x = !!ensym(g2), y = !!ensym(y), fill = !!ensym(g1)),
+                             outlier.shape = NA, 
+                             alpha = .7, 
+                             width = .6, 
+                             size=0.6,
+                             colour = "black", show.legend = FALSE) +
+                geom_point(data = sumrepdat, 
+                           aes(x = !!ensym(g2), y = mean,
+                               group = !!ensym(g1), color = !!ensym(g1)),
+                           shape = 5, size=3, show.legend = FALSE,
+                           position = position_dodge(width = .6), color='grey') +
+                scale_x_discrete(labels = xticklabs) +
+                scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
+                scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, alpha = .8,
+                                     direction = -1, labels = legend_labels) +
+                ggtitle(title) + xlab(xlab) + ylab(ylab) + 
+                guides(fill=guide_legend(title=legend_title, reverse = FALSE,
+                                         title.position = 'left', label.position = 'right')) +
+                theme_cowplot(font_size = 16, font_family = 'Calibri') +
+                theme(legend.position = legend_position,
+                      legend.key.size = unit(0.5,'cm'))
+        }
         
 }
+
+
+
+# Generate raincloud
+# ggplot(data, aes_string(x = g2, y = y, fill = g1)) +
+#         geom_flat_violin(aes_string(fill = g1),
+#                          position = position_nudge(x = .1, y = 0), 
+#                          adjust = 1.5, 
+#                          trim = FALSE, 
+#                          alpha = .5, 
+#                          colour = 'black') +
+#         geom_jitter(aes_string(x = paste('as.numeric(',g2,')-.1', sep=''), y = y, colour = g1),
+#                     size = 2,
+#                     shape = 19,
+#                     width = .04,
+#                     alpha = .8, show.legend = FALSE) +
+#         geom_boxplot(aes_string(x = g2, y = y, fill = g1),
+#                      outlier.shape = NA, 
+#                      alpha = .7, 
+#                      width = .1, 
+#                      colour = "black", show.legend = FALSE) +
+#         geom_line(data = sumrepdat,
+#                   aes_string(x = paste('as.numeric(',g2,')+.15'), y = 'mean', group = g1, color = g1),
+#                   linetype = 3, size = .9, show.legend = FALSE) +
+#         geom_point(data = sumrepdat, 
+#                    aes_string(x = paste('as.numeric(',g2,')+.1', sep=''), y = 'mean',
+#                               group = g1, color = g1), shape = 18, size=3, show.legend = FALSE) +
+#         geom_errorbar(data = sumrepdat, 
+#                       aes_string(x = paste('as.numeric(',g2,')+.1',sep=''), y = 'mean',
+#                                  ymin = 'mean-se', ymax = 'mean+se', group = g1, color = g1),
+#                       width = .07, show.legend = FALSE) +
+#         scale_x_discrete(labels = xticklabs) +
+#         scale_colour_viridis_d(option=color_scheme, begin = .1, end = .6, direction = -1) +
+#         scale_fill_viridis_d(option=color_scheme, begin = .1, end = .6, alpha = .8,
+#                              direction = -1, labels = legend_labels) +
+#         ggtitle(title) + xlab(xlab) + ylab(ylab) + 
+#         guides(fill=guide_legend(title=legend_title, reverse = FALSE)) + 
+#         coord_cartesian(xlim = c(1.2, NA), clip = "off") +
+#         theme_cowplot(font_size = 20)
